a74a26b51c68d681e10d541de528bb93fb9a17cf
Erik Schnetter <schnetter@gmail.com>
2019-10-10 11:23:19 -0400

    HydroToyAMReX: Make fluxes work

--- HydroToyAMReX/src/hydrotoy.cxx
+++ HydroToyAMReX/src/hydrotoy.cxx
@@ -26,7 +26,7 @@ extern "C" void HydroToyAMReX_Initialize(CCTK_ARGUMENTS) {
 
   if (CCTK_EQUALS(setup, "equilibrium")) {
 
-    Loop::loop_all<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
       rho[p.idx] = 1.0;
       momx[p.idx] = 0.0;
       momy[p.idx] = 0.0;
@@ -36,12 +36,49 @@ extern "C" void HydroToyAMReX_Initialize(CCTK_ARGUMENTS) {
 
   } else if (CCTK_EQUALS(setup, "sound wave")) {
 
-    Loop::loop_all<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
       rho[p.idx] = 1.0;
       momx[p.idx] = 0.0 + amplitude * sin(M_PI * p.x);
       momy[p.idx] = 0.0;
       momz[p.idx] = 0.0;
-      etot[p.idx] = 1.0;
+      etot[p.idx] = 1.0; // should add kinetic energy here
+    });
+
+  } else if (CCTK_EQUALS(setup, "shock tube")) {
+
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      if (p.x <= 0.0) {
+        rho[p.idx] = 2.0;
+        momx[p.idx] = 0.0;
+        momy[p.idx] = 0.0;
+        momz[p.idx] = 0.0;
+        etot[p.idx] = 2.0;
+      } else {
+        rho[p.idx] = 1.0;
+        momx[p.idx] = 0.0;
+        momy[p.idx] = 0.0;
+        momz[p.idx] = 0.0;
+        etot[p.idx] = 1.0;
+      }
+    });
+
+  } else if (CCTK_EQUALS(setup, "spherical shock")) {
+
+    Loop::loop_int<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+      CCTK_REAL r2 = pow(p.x, 2) + pow(p.y, 2) + pow(p.z, 2);
+      if (r2 <= pow(shock_radius, 2)) {
+        rho[p.idx] = 2.0;
+        momx[p.idx] = 0.0;
+        momy[p.idx] = 0.0;
+        momz[p.idx] = 0.0;
+        etot[p.idx] = 2.0;
+      } else {
+        rho[p.idx] = 1.0;
+        momx[p.idx] = 0.0;
+        momy[p.idx] = 0.0;
+        momz[p.idx] = 0.0;
+        etot[p.idx] = 1.0;
+      }
     });
 
   } else {
@@ -155,12 +192,14 @@ extern "C" void HydroToyAMReX_Fluxes(CCTK_ARGUMENTS) {
 
   Loop::loop_int<0, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
     auto mkflux{[&](auto &u, auto f) {
-      auto lambda_m = -dx / dt;
-      auto lambda_p = dx / dt;
-      auto var_m = u(p.I - p.DI(0));
-      auto var_p = u(p.I);
-      auto flux_m = f(p.I - p.DI(0));
-      auto flux_p = f(p.I);
+      auto I_m = p.I - p.DI(0);
+      auto I_p = p.I;
+      auto lambda_m = 1.0;
+      auto lambda_p = -1.0;
+      auto var_m = u(I_m);
+      auto var_p = u(I_p);
+      auto flux_m = f(I_m);
+      auto flux_p = f(I_p);
       return llf(lambda_m, lambda_p, var_m, var_p, flux_m, flux_p);
     }};
 
@@ -177,12 +216,14 @@ extern "C" void HydroToyAMReX_Fluxes(CCTK_ARGUMENTS) {
 
   Loop::loop_int<1, 0, 1>(cctkGH, [&](const Loop::PointDesc &p) {
     auto mkflux{[&](auto &u, auto f) {
-      auto lambda_m = -dy / dt;
-      auto lambda_p = dy / dt;
-      auto var_m = u(p.I - p.DI(1));
-      auto var_p = u(p.I);
-      auto flux_m = f(p.I - p.DI(1));
-      auto flux_p = f(p.I);
+      auto I_m = p.I - p.DI(1);
+      auto I_p = p.I;
+      auto lambda_m = 1.0;
+      auto lambda_p = -1.0;
+      auto var_m = u(I_m);
+      auto var_p = u(I_p);
+      auto flux_m = f(I_m);
+      auto flux_p = f(I_p);
       return llf(lambda_m, lambda_p, var_m, var_p, flux_m, flux_p);
     }};
 
@@ -199,12 +240,14 @@ extern "C" void HydroToyAMReX_Fluxes(CCTK_ARGUMENTS) {
 
   Loop::loop_int<1, 1, 0>(cctkGH, [&](const Loop::PointDesc &p) {
     auto mkflux{[&](auto &u, auto f) {
-      auto lambda_m = -dz / dt;
-      auto lambda_p = dz / dt;
-      auto var_m = u(p.I - p.DI(2));
-      auto var_p = u(p.I);
-      auto flux_m = f(p.I - p.DI(2));
-      auto flux_p = f(p.I);
+      auto I_m = p.I - p.DI(2);
+      auto I_p = p.I;
+      auto lambda_m = 1.0;
+      auto lambda_p = -1.0;
+      auto var_m = u(I_m);
+      auto var_p = u(I_p);
+      auto flux_m = f(I_m);
+      auto flux_p = f(I_p);
       return llf(lambda_m, lambda_p, var_m, var_p, flux_m, flux_p);
     }};
 
@@ -217,6 +260,68 @@ extern "C" void HydroToyAMReX_Fluxes(CCTK_ARGUMENTS) {
 
     fzetot_(p.I) = mkflux(
         etot_, [&](auto I) { return (etot_(I) + press_(I)) * velz_(I); });
+  });
+}
+
+extern "C" void HydroToyAMReX_Evolve1(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  const CCTK_REAL dt = CCTK_DELTA_TIME;
+  const CCTK_REAL dx = CCTK_DELTA_SPACE(0);
+  const CCTK_REAL dy = CCTK_DELTA_SPACE(1);
+  const CCTK_REAL dz = CCTK_DELTA_SPACE(2);
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> rho_p_(cctkGH, rho_p);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momx_p_(cctkGH, momx_p);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momy_p_(cctkGH, momy_p);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momz_p_(cctkGH, momz_p);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> etot_p_(cctkGH, etot_p);
+
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxrho_(cctkGH, fxrho);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomx_(cctkGH, fxmomx);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomy_(cctkGH, fxmomy);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomz_(cctkGH, fxmomz);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxetot_(cctkGH, fxetot);
+
+  const Loop::GF3D<const CCTK_REAL, 1, 0, 1> fyrho_(cctkGH, fyrho);
+  const Loop::GF3D<const CCTK_REAL, 1, 0, 1> fymomx_(cctkGH, fymomx);
+  const Loop::GF3D<const CCTK_REAL, 1, 0, 1> fymomy_(cctkGH, fymomy);
+  const Loop::GF3D<const CCTK_REAL, 1, 0, 1> fymomz_(cctkGH, fymomz);
+  const Loop::GF3D<const CCTK_REAL, 1, 0, 1> fyetot_(cctkGH, fyetot);
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 0> fzrho_(cctkGH, fzrho);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 0> fzmomx_(cctkGH, fzmomx);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 0> fzmomy_(cctkGH, fzmomy);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 0> fzmomz_(cctkGH, fzmomz);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 0> fzetot_(cctkGH, fzetot);
+
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> rho_(cctkGH, rho);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> momx_(cctkGH, momx);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> momy_(cctkGH, momy);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> momz_(cctkGH, momz);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> etot_(cctkGH, etot);
+
+  // Transport
+  // dt rho + d_i (rho vel^i) = 0
+  // dt mom_j + d_i (mom_j vel^i) = 0
+  // dt etot + d_i (etot vel^i) = 0
+
+  Loop::loop_all<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+    auto mkupdate{[&](auto &fx, auto &fy, auto &fz) {
+      return 0.5 * dt *
+             ((fx(p.I + p.DI(0)) - fx(p.I)) / dx +
+              (fy(p.I + p.DI(1)) - fy(p.I)) / dy +
+              (fz(p.I + p.DI(2)) - fz(p.I)) / dz);
+    }};
+
+    rho_(p.I) = rho_p_(p.I) - mkupdate(fxrho_, fyrho_, fzrho_);
+
+    momx_(p.I) = momx_p_(p.I) - mkupdate(fxmomx_, fymomx_, fzmomx_);
+    momy_(p.I) = momy_p_(p.I) - mkupdate(fxmomy_, fymomy_, fzmomy_);
+    momz_(p.I) = momz_p_(p.I) - mkupdate(fxmomz_, fymomz_, fzmomz_);
+
+    etot_(p.I) = etot_p_(p.I) - mkupdate(fxetot_, fyetot_, fzetot_);
   });
 }
 
@@ -287,6 +392,46 @@ extern "C" void HydroToyAMReX_EstimateError(CCTK_ARGUMENTS) {
 
   Loop::loop_int<1, 1, 1>(
       cctkGH, [&](const Loop::PointDesc &p) { regrid_error[p.idx] = 0.0; });
+}
+
+extern "C" void HydroToyAMReX_Output(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> rho_(cctkGH, rho);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momx_(cctkGH, momx);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momy_(cctkGH, momy);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> momz_(cctkGH, momz);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> etot_(cctkGH, etot);
+
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxrho_(cctkGH, fxrho);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomx_(cctkGH, fxmomx);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomy_(cctkGH, fxmomy);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxmomz_(cctkGH, fxmomz);
+  const Loop::GF3D<const CCTK_REAL, 0, 1, 1> fxetot_(cctkGH, fxetot);
+
+  Loop::loop_all<1, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+    if (p.j == 0 && p.k == 0) {
+#pragma omp critical(HydroToyAMReX_Output)
+      {
+        cout << "[" << p.i << "," << p.j << "," << p.k << "] (" << p.x << ","
+             << p.y << "," << p.z << ") rho=" << rho_(p.I)
+             << " momx=" << momx_(p.I) << " etot=" << etot_(p.I) << "\n";
+      }
+    }
+  });
+
+  Loop::loop_all<0, 1, 1>(cctkGH, [&](const Loop::PointDesc &p) {
+    if (p.j == 0 && p.k == 0) {
+#pragma omp critical(HydroToyAMReX_Output)
+      {
+        cout << "[" << p.i << "," << p.j << "," << p.k << "] (" << p.x << ","
+             << p.y << "," << p.z << ") fxrho=" << fxrho_(p.I)
+             << " fxmomx=" << fxmomx_(p.I) << " fxetot=" << fxetot_(p.I)
+             << "\n";
+      }
+    }
+  });
 }
 
 } // namespace HydroToyAMReX
