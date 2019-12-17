15bbffec654ba3f5b06e4da41b1f10b7d7b607c8
Roland Haas <rhaas@tapir.caltech.edu>
2019-12-16 23:50:22 -0500

    BaikalX: add NRPy+ version 9832d44

---
+++ BaikalX/doc/nrpytutorial/BSSN/ADMBSSN_tofrom_4metric.py
@@ -0,0 +1,156 @@
+# As documented in the NRPy+ tutorial module
+#   Tutorial-ADMBSSN_tofrom_4metric.ipynb,
+#   this module will construct expressions for
+#   ADM or BSSN quantities in terms of the
+#   4-metric g4DD, and g4DD/g4UU in terms of
+#   ADM/BSSN quantities.
+
+# Author: Zachariah B. Etienne
+#         zachetie **at** gmail **dot* com
+
+import sympy as sp                # SymPy: The Python computer algebra package upon which NRPy+ depends
+import indexedexp as ixp          # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
+import sys                        # Standard Python modules for multiplatform OS-level functions
+
+def setup_ADM_quantities(inputvars):
+    if inputvars == "ADM":
+        gammaDD = ixp.declarerank2("gammaDD", "sym01")
+        betaU = ixp.declarerank1("betaU")
+        alpha = sp.symbols("alpha", real=True)
+    elif inputvars == "BSSN":
+        import BSSN.ADM_in_terms_of_BSSN as AitoB
+
+        # Construct gamma_{ij} in terms of cf & gammabar_{ij}
+        AitoB.ADM_in_terms_of_BSSN()
+        gammaDD = AitoB.gammaDD
+        # Next construct beta^i in terms of vet^i and reference metric quantities
+        import BSSN.BSSN_quantities as Bq
+
+        Bq.BSSN_basic_tensors()
+        betaU = Bq.betaU
+        alpha = sp.symbols("alpha", real=True)
+    else:
+        print("inputvars = " + str(inputvars) + " not supported. Please choose ADM or BSSN.")
+        sys.exit(1)
+    return gammaDD,betaU,alpha
+
+# g_{mu nu} in terms of BSSN (if inputvars=="BSSN") or ADM (if inputvars=="ADM") variables.
+def g4DD_ito_BSSN_or_ADM(inputvars,gammaDD=None,betaU=None,alpha=None):
+    # Step 0: Declare g4DD as globals, to make interfacing with other modules/functions easier
+    global g4DD
+
+    # Step 1: Set gammaDD, betaU, and alpha if not already input.
+    if gammaDD==None and betaU==None and alpha==None:
+        gammaDD,betaU,alpha = setup_ADM_quantities(inputvars)
+
+    # Step 2: Compute g4DD = g_{mu nu}:
+    # To get \gamma_{\mu \nu} = gamma4DD[mu][nu], we'll need to construct the 4-metric, using Eq. 2.122 in B&S:
+    g4DD = ixp.zerorank2(DIM=4)
+
+    # Step 2.a: Compute beta_i via Eq. 2.121 in B&S
+    betaD = ixp.zerorank1()
+    for i in range(3):
+        for j in range(3):
+            betaD[i] += gammaDD[i][j] * betaU[j]
+
+    # Step 2.b: Compute beta_i beta^i, the beta contraction.
+    beta2 = sp.sympify(0)
+    for i in range(3):
+        beta2 += betaU[i] * betaD[i]
+
+    # Step 2.c: Construct g4DD via Eq. 2.122 in B&S
+    g4DD[0][0] = -alpha ** 2 + beta2
+    for mu in range(1, 4):
+        g4DD[mu][0] = g4DD[0][mu] = betaD[mu - 1]
+    for mu in range(1, 4):
+        for nu in range(1, 4):
+            g4DD[mu][nu] = gammaDD[mu - 1][nu - 1]
+
+# g^{mu nu} in terms of BSSN (if inputvars=="BSSN") or ADM (if inputvars=="ADM") variables.
+def g4UU_ito_BSSN_or_ADM(inputvars,gammaDD=None,betaU=None,alpha=None, gammaUU=None):
+    # Step 0: Declare g4UU as globals, to make interfacing with other modules/functions easier
+    global g4UU
+
+    # Step 1: Set gammaDD, betaU, and alpha if not already input.
+    if gammaDD==None and betaU==None and alpha==None:
+        gammaDD,betaU,alpha = setup_ADM_quantities(inputvars)
+
+    # Step 2: Compute g4UU = g_{mu nu}:
+    # To get \gamma^{\mu \nu} = gamma4UU[mu][nu], we'll need to use Eq. 2.119 in B&S.
+    g4UU = ixp.zerorank2(DIM=4)
+
+    # Step 3: Construct g4UU = g^{mu nu}
+    # Step 3.a: Compute gammaUU based on provided gammaDD:
+    if gammaUU==None:
+        gammaUU, gammaDET = ixp.symm_matrix_inverter3x3(gammaDD)
+
+    # Then evaluate g4UU:
+    g4UU = ixp.zerorank2(DIM=4)
+
+    g4UU[0][0] = -1 / alpha ** 2
+    for mu in range(1, 4):
+        g4UU[0][mu] = g4UU[mu][0] = betaU[mu - 1] / alpha ** 2
+    for mu in range(1, 4):
+        for nu in range(1, 4):
+            g4UU[mu][nu] = gammaUU[mu - 1][nu - 1] - betaU[mu - 1] * betaU[nu - 1] / alpha ** 2
+
+# BSSN (if inputvars=="BSSN") or ADM (if inputvars=="ADM") metric variables in terms of g_{mu nu}
+def BSSN_or_ADM_ito_g4DD(inputvars,g4DD=None):
+    # Step 0: Declare output variables as globals, to make interfacing with other modules/functions easier
+    if inputvars == "ADM":
+        global gammaDD, betaU, alpha
+    elif inputvars == "BSSN":
+        global hDD, cf, vetU, alpha
+    else:
+        print("inputvars = " + str(inputvars) + " not supported. Please choose ADM or BSSN.")
+        sys.exit(1)
+
+    # Step 1: declare g4DD as symmetric rank-4 tensor:
+    g4DD_is_input_into_this_function = True
+    if g4DD == None:
+        g4DD = ixp.declarerank2("g4DD", "sym01", DIM=4)
+        g4DD_is_input_into_this_function = False
+
+    # Step 2: Compute gammaDD & betaD
+    betaD = ixp.zerorank1()
+    gammaDD = ixp.zerorank2()
+    for i in range(3):
+        betaD[i] = g4DD[0][i]
+        for j in range(3):
+            gammaDD[i][j] = g4DD[i + 1][j + 1]
+
+    # Step 3: Compute betaU
+    # Step 3.a: Compute gammaUU based on provided gammaDD
+    gammaUU, gammaDET = ixp.symm_matrix_inverter3x3(gammaDD)
+
+    # Step 3.b: Use gammaUU to raise betaU
+    betaU = ixp.zerorank1()
+    for i in range(3):
+        for j in range(3):
+            betaU[i] += gammaUU[i][j] * betaD[j]
+
+    # Step 4:   Compute alpha = sqrt(beta^2 - g_{00}):
+    # Step 4.a: Compute beta^2 = beta^k beta_k:
+    beta_squared = sp.sympify(0)
+    for k in range(3):
+        beta_squared += betaU[k] * betaD[k]
+
+    # Step 4.b: alpha = sqrt(beta^2 - g_{00}):
+    if g4DD_is_input_into_this_function == False:
+        alpha = sp.sqrt(sp.simplify(beta_squared) - g4DD[0][0])
+    else:
+        alpha = sp.sqrt(beta_squared - g4DD[0][0])
+
+    # Step 5: If inputvars == "ADM", we are finished. Return.
+    if inputvars == "ADM":
+        return
+
+    # Step 6: If inputvars == "BSSN", convert ADM to BSSN
+    import BSSN.BSSN_in_terms_of_ADM as BitoA
+    dummyBU = ixp.zerorank1()
+    BitoA.gammabarDD_hDD(gammaDD)
+    BitoA.cf_from_gammaDD(gammaDD)
+    BitoA.betU_vetU(betaU, dummyBU)
+    hDD  = BitoA.hDD
+    cf   = BitoA.cf
+    vetU = BitoA.vetU
---
+++ BaikalX/doc/nrpytutorial/BSSN/ADM_in_terms_of_BSSN.py
@@ -0,0 +1,139 @@
+# As documented in the NRPy+ tutorial module
+#   Tutorial-ADM_in_terms_of_BSSN.ipynb,
+#   this module will construct expressions for ADM
+#   quantities in terms of BSSN quantities.
+
+# Author: Zachariah B. Etienne
+#         zachetie **at** gmail **dot* com
+
+# Step 1.a: import all needed modules from NRPy+:
+from outputC import *             # NRPy+: Core C code output module
+import sympy as sp                # SymPy: The Python computer algebra package upon which NRPy+ depends
+import indexedexp as ixp          # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
+import reference_metric as rfm    # NRPy+: Reference metric support
+import sys                        # Standard Python module for multiplatform OS-level functions
+
+def ADM_in_terms_of_BSSN():
+    global gammaDD, gammaDDdD, gammaDDdDD, gammaUU, detgamma, GammaUDD, KDD, KDDdD
+    # Step 1.c: Given the chosen coordinate system, set up
+    #           corresponding reference metric and needed
+    #           reference metric quantities
+    # The following function call sets up the reference metric
+    #    and related quantities, including rescaling matrices ReDD,
+    #    ReU, and hatted quantities.
+    rfm.reference_metric()
+
+    # Step 1.d: Set spatial dimension (must be 3 for BSSN, as BSSN is
+    #           a 3+1-dimensional decomposition of the general
+    #           relativistic field equations)
+    DIM = 3
+
+    # Step 1.e: Import all basic (unrescaled) BSSN scalars & tensors
+    import BSSN.BSSN_quantities as Bq
+    Bq.BSSN_basic_tensors()
+    gammabarDD = Bq.gammabarDD
+    cf         = Bq.cf
+    AbarDD     = Bq.AbarDD
+    trK        = Bq.trK
+
+    Bq.gammabar__inverse_and_derivs()
+    gammabarDD_dD  = Bq.gammabarDD_dD
+    gammabarDD_dDD = Bq.gammabarDD_dDD
+
+    Bq.AbarUU_AbarUD_trAbar_AbarDD_dD()
+    AbarDD_dD = Bq.AbarDD_dD
+
+    # Step 2: The ADM three-metric gammaDD and its
+    #         derivatives in terms of BSSN quantities.
+    gammaDD = ixp.zerorank2()
+
+    exp4phi = sp.sympify(0)
+    if par.parval_from_str("EvolvedConformalFactor_cf") == "phi":
+        exp4phi = sp.exp(4 * cf)
+    elif par.parval_from_str("EvolvedConformalFactor_cf") == "chi":
+        exp4phi = (1 / cf)
+    elif par.parval_from_str("EvolvedConformalFactor_cf") == "W":
+        exp4phi = (1 / cf ** 2)
+    else:
+        print("Error EvolvedConformalFactor_cf type = \"" + par.parval_from_str("EvolvedConformalFactor_cf") + "\" unknown.")
+        sys.exit(1)
+
+    for i in range(DIM):
+        for j in range(DIM):
+            gammaDD[i][j] = exp4phi * gammabarDD[i][j]
+
+    # Step 2.a: Derivatives of $e^{4\phi}$
+    phidD = ixp.zerorank1()
+    phidDD = ixp.zerorank2()
+    cf_dD  = ixp.declarerank1("cf_dD")
+    cf_dDD = ixp.declarerank2("cf_dDD","sym01")
+    if par.parval_from_str("EvolvedConformalFactor_cf") == "phi":
+        for i in range(DIM):
+            phidD[i]  = cf_dD[i]
+            for j in range(DIM):
+                phidDD[i][j] = cf_dDD[i][j]
+    elif par.parval_from_str("EvolvedConformalFactor_cf") == "chi":
+        for i in range(DIM):
+            phidD[i]  = -sp.Rational(1,4)*exp4phi*cf_dD[i]
+            for j in range(DIM):
+                phidDD[i][j] = sp.Rational(1,4)*( exp4phi**2*cf_dD[i]*cf_dD[j] - exp4phi*cf_dDD[i][j] )
+    elif par.parval_from_str("EvolvedConformalFactor_cf") == "W":
+        exp2phi = (1 / cf)
+        for i in range(DIM):
+            phidD[i]  = -sp.Rational(1,2)*exp2phi*cf_dD[i]
+            for j in range(DIM):
+                phidDD[i][j] = sp.Rational(1,2)*( exp4phi*cf_dD[i]*cf_dD[j] - exp2phi*cf_dDD[i][j] )
+    else:
+        print("Error EvolvedConformalFactor_cf type = \""+par.parval_from_str("EvolvedConformalFactor_cf")+"\" unknown.")
+        sys.exit(1)
+
+    exp4phidD  = ixp.zerorank1()
+    exp4phidDD = ixp.zerorank2()
+    for i in range(DIM):
+        exp4phidD[i] = 4*exp4phi*phidD[i]
+        for j in range(DIM):
+            exp4phidDD[i][j] = 16*exp4phi*phidD[i]*phidD[j] + 4*exp4phi*phidDD[i][j]
+
+    # Step 2.b: Derivatives of gammaDD, the ADM three-metric
+    gammaDDdD = ixp.zerorank3()
+    gammaDDdDD = ixp.zerorank4()
+
+    for i in range(DIM):
+        for j in range(DIM):
+            for k in range(DIM):
+                gammaDDdD[i][j][k] = exp4phidD[k] * gammabarDD[i][j] + exp4phi * gammabarDD_dD[i][j][k]
+                for l in range(DIM):
+                    gammaDDdDD[i][j][k][l] = exp4phidDD[k][l] * gammabarDD[i][j] + \
+                                             exp4phidD[k] * gammabarDD_dD[i][j][l] + \
+                                             exp4phidD[l] * gammabarDD_dD[i][j][k] + \
+                                             exp4phi * gammabarDD_dDD[i][j][k][l]
+
+    # Step 2.c: 3-Christoffel symbols associated with ADM 3-metric gammaDD
+    # Step 2.c.i: First compute the inverse 3-metric gammaUU:
+    gammaUU, detgamma = ixp.symm_matrix_inverter3x3(gammaDD)
+
+    GammaUDD = ixp.zerorank3()
+
+    for i in range(DIM):
+        for j in range(DIM):
+            for k in range(DIM):
+                for l in range(DIM):
+                    GammaUDD[i][j][k] += sp.Rational(1,2)*gammaUU[i][l]* \
+                                    (gammaDDdD[l][j][k] + gammaDDdD[l][k][j] - gammaDDdD[j][k][l])
+                    
+    # Step 3: Define ADM extrinsic curvature KDD and
+    #         its first spatial derivatives KDDdD
+    #         in terms of BSSN quantities
+    KDD = ixp.zerorank2()
+
+    for i in range(DIM):
+        for j in range(DIM):
+            KDD[i][j] = exp4phi * AbarDD[i][j] + sp.Rational(1, 3) * gammaDD[i][j] * trK
+
+    KDDdD = ixp.zerorank3()
+    trK_dD = ixp.declarerank1("trK_dD")
+    for i in range(DIM):
+        for j in range(DIM):
+            for k in range(DIM):
+                KDDdD[i][j][k] = exp4phidD[k] * AbarDD[i][j] + exp4phi * AbarDD_dD[i][j][k] + \
+                                 sp.Rational(1, 3) * (gammaDDdD[i][j][k] * trK + gammaDD[i][j] * trK_dD[k])
