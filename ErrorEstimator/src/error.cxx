ce951b96cdca59b3af9b45e892564edaa2e49d41
Erik Schnetter <schnetter@gmail.com>
2020-01-02 12:36:20 -0500

    ErrorEstimator: Sample neighbourhood of grid points for errors to avoid missing refining near the origin

--- ErrorEstimator/src/error.cxx
+++ ErrorEstimator/src/error.cxx
@@ -4,6 +4,7 @@
 #include <cctk_Arguments_Checked.h>
 #include <cctk_Parameters.h>
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 
@@ -29,15 +30,28 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_ErrorEstimator_Estimate;
   DECLARE_CCTK_PARAMETERS;
 
+  const CCTK_REAL dx = CCTK_DELTA_SPACE(0);
+  const CCTK_REAL dy = CCTK_DELTA_SPACE(1);
+  const CCTK_REAL dz = CCTK_DELTA_SPACE(2);
+
+  const CCTK_REAL scalefactor = scale_by_resolution ? cbrt(dx * dy * dz) : 1;
+
   const GF3D<CCTK_REAL, 1, 1, 1> regrid_error_(cctkGH, regrid_error);
 
-  const CCTK_REAL scalefactor = scale_by_resolution ?
-    pow(CCTK_DELTA_SPACE(0)*CCTK_DELTA_SPACE(1)*CCTK_DELTA_SPACE(2), 1./3.) :
-    1.;
-
   loop_int<1, 1, 1>(cctkGH, [&](const PointDesc &p) {
-    auto r = sqrt(pow2(p.x) + pow2(p.y) + pow2(p.z));
-    regrid_error_(p.I) = scalefactor / fmax(r, CCTK_REAL(1.0e-10));
+    CCTK_REAL maxerr = 0;
+    // Sample the neighbourhood of this point
+    for (int k = -1; k <= +1; ++k) {
+      for (int j = -1; j <= +1; ++j) {
+        for (int i = -1; i <= +1; ++i) {
+          const auto r = sqrt(pow2(p.x + i * dx / 2) + pow2(p.y + j * dy / 2) +
+                              pow2(p.z + k * dz / 2));
+          const auto err = scalefactor / fmax(r, epsilon);
+          maxerr = fmax(maxerr, err);
+        }
+      }
+    }
+    regrid_error_(p.I) = maxerr;
   });
 }
 
