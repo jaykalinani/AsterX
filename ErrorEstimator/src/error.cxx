b34d7937c0e791b4e5c67e6d59bcc63500f09626
Erik Schnetter <schnetter@gmail.com>
2020-01-08 16:08:54 -0500

    ErrorEstimator: New parameter "region_shape" to refine either spheres or cubes

--- ErrorEstimator/src/error.cxx
+++ ErrorEstimator/src/error.cxx
@@ -12,8 +12,22 @@ namespace ErrorEstimator {
 using namespace Loop;
 using namespace std;
 
-namespace {
-template <typename T> T pow2(T x) { return x * x; }
+template <typename T> constexpr T pow2(const T x) { return x * x; }
+
+template <typename T> constexpr T fmax3(const T x, const T y, const T z) {
+  return fmax(fmax(x, y), z);
+}
+
+enum class shape_t { sphere, cube };
+
+template <typename T>
+constexpr T radius_sphere(const T x, const T y, const T z) {
+  return sqrt(pow2(x) + pow2(y) + pow2(z));
+}
+
+template <typename T> constexpr T radius_cube(const T x, const T y, const T z) {
+  return fmax3(fabs(x), fabs(y), fabs(z));
+}
 
 template <typename T, int CI, int CJ, int CK>
 T lap(const GF3D<const T, CI, CJ, CK> &var, const vect<int, dim> &I) {
@@ -24,7 +38,6 @@ T lap(const GF3D<const T, CI, CJ, CK> &var, const vect<int, dim> &I) {
          fabs(var(I - DJ) - 2 * var(I) + var(I + DJ)) +
          fabs(var(I - DK) - 2 * var(I) + var(I + DK));
 }
-} // namespace
 
 extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_ErrorEstimator_Estimate;
@@ -38,14 +51,24 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
 
   const GF3D<CCTK_REAL, 1, 1, 1> regrid_error_(cctkGH, regrid_error);
 
+  typedef CCTK_REAL radius_t(CCTK_REAL x, CCTK_REAL y, CCTK_REAL z);
+  radius_t *const radius = [](const char *const sh) {
+    if (CCTK_EQUALS(sh, "sphere"))
+      return (radius_t *)radius_sphere;
+    else if (CCTK_EQUALS(sh, "cube"))
+      return (radius_t *)radius_cube;
+    else
+      assert(0);
+  }(region_shape);
+
   loop_int<1, 1, 1>(cctkGH, [&](const PointDesc &p) {
     CCTK_REAL maxerr = 0;
     // Sample the neighbourhood of this point
     for (int k = -1; k <= +1; ++k) {
       for (int j = -1; j <= +1; ++j) {
         for (int i = -1; i <= +1; ++i) {
-          const auto r = sqrt(pow2(p.x + i * dx / 2) + pow2(p.y + j * dy / 2) +
-                              pow2(p.z + k * dz / 2));
+          const auto r =
+              radius(p.x + i * dx / 2, p.y + j * dy / 2, p.z + k * dz / 2);
           const auto err = scalefactor / fmax(r, epsilon);
           maxerr = fmax(maxerr, err);
         }
