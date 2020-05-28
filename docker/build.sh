3030fb920bdb25a21e3501e93bc37c519f1a9cf2
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-05-28 15:03:57 -0500

    Redo this image to make use of Erik's azure-pipelines ubuntu image

--- docker/build.sh
+++ docker/build.sh
@@ -1,19 +1,18 @@
-cd
-grep 'module load' .bashrc > /dev/null
-if [ $? = 1 ]
+PROCS=$(lscpu | grep CPU.s.: | head -1 | cut -d: -f2)
+if [ "$USE_PROCS" = "" ]
 then
-  echo module load mpi >> .bashrc
+  USE_PROCS=$(($PROCS/2))
 fi
-source .bashrc
+cd
 if [ ! -d cactusamrex ]
 then
-  git clone https://stevenrbrandt@bitbucket.org/eschnett/cactusamrex.git
+  git clone https://bitbucket.org/eschnett/cactusamrex.git
 fi
 cd /home/jovyan/cactusamrex
 git pull
 if [ ! -x GetComponents ]
 then
-  curl -kLO https://raw.githubusercontent.com/gridaphobe/CRL/ET_2019_03/GetComponents
+  wget https://raw.githubusercontent.com/gridaphobe/CRL/ET_2019_03/GetComponents
   chmod a+x GetComponents
   ./GetComponents --parallel azure-pipelines/carpetx.th
 fi
@@ -24,6 +23,5 @@ if [ ! -r repos/simfactory2/mdb/machines/docker.ini ]
 then
   ./simfactory/bin/sim setup-silent
 fi
-#COPY amrex.cfg ./
-./simfactory/bin/sim build -j2 --thornlist ../azure-pipelines/carpetx.th --optionlist /amrex.cfg
+./simfactory/bin/sim build -j ${USE_PROCS} --thornlist ../azure-pipelines/carpetx.th --optionlist ../azure-pipelines/ubuntu.cfg 2>&1 | tee make.out
 #./exe/cactus_sim ./repos/cactusamrex/AMReX/par/amrex.par
