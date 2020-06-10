71b5bb2ad9777fa850e8e6ba0a5f86a0abd460f7
Erik Schnetter <schnetter@gmail.com>
2020-06-09 22:40:40 -0400

    CarpetX: Trivial cleanup

--- CarpetX/src/prolongate_3d_rf2.cxx
+++ CarpetX/src/prolongate_3d_rf2.cxx
@@ -5,8 +5,8 @@
 #include <omp.h>
 #else
 extern "C" {
-static inline int omp_get_thread_num(void) { return 0; }
 static inline int omp_get_num_threads(void) { return 1; }
+static inline int omp_get_thread_num(void) { return 0; }
 }
 #endif
 
--- CarpetX/src/schedule.cxx
+++ CarpetX/src/schedule.cxx
@@ -23,10 +23,10 @@
 #include <omp.h>
 #else
 extern "C" {
-static inline int omp_in_parallel(void) { return 0; }
-static inline int omp_get_thread_num(void) { return 0; }
-static inline int omp_get_num_threads(void) { return 1; }
 static inline int omp_get_max_threads(void) { return 1; }
+static inline int omp_get_num_threads(void) { return 1; }
+static inline int omp_get_thread_num(void) { return 0; }
+static inline int omp_in_parallel(void) { return 0; }
 }
 #endif
 #include <mpi.h>
