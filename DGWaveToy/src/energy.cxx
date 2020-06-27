078bd7b43a461180b3560dfef5953fab502bd70a
Erik Schnetter <schnetter@gmail.com>
2020-06-27 19:20:51 -0400

    DGWaveToy: Implement WaveToy with DG derivatives

---
+++ DGWaveToy/src/energy.cxx
@@ -0,0 +1,91 @@
+#include <dg.hxx>
+#include <loop.hxx>
+#include <vect.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments_Checked.h>
+#include <cctk_Parameters.h>
+
+#include <cassert>
+#include <cmath>
+
+namespace DGWaveToy {
+using namespace DG;
+using namespace Arith;
+using namespace std;
+
+using Loop::dim;
+
+extern "C" void DGWaveToy_Energy(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_DGWaveToy_Energy;
+  DECLARE_CCTK_PARAMETERS;
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> ft_rhs_(cctkGH, ft_rhs);
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fx_(cctkGH, fx);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fy_(cctkGH, fy);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fz_(cctkGH, fz);
+
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> eps_(cctkGH, eps);
+
+  const Loop::GridDescBase grid(cctkGH);
+
+  switch (dg_npoints) {
+
+  case 2: {
+    constexpr int N = 2;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      eps_(p.I) = (pow(ft_rhs_(p.I), 2) + pow(dfx[0], 2) + pow(dfy[1], 2) +
+                   pow(dfz[2], 2));
+    });
+    break;
+  }
+
+  case 4: {
+    constexpr int N = 4;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      eps_(p.I) = (pow(ft_rhs_(p.I), 2) + pow(dfx[0], 2) + pow(dfy[1], 2) +
+                   pow(dfz[2], 2));
+    });
+    break;
+  }
+
+  case 8: {
+    constexpr int N = 8;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      eps_(p.I) = (pow(ft_rhs_(p.I), 2) + pow(dfx[0], 2) + pow(dfy[1], 2) +
+                   pow(dfz[2], 2));
+    });
+    break;
+  }
+
+  default:
+    assert(0);
+  }
+}
+
+extern "C" void DGWaveToy_EnergyBoundaries(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_DGWaveToy_EnergyBoundaries;
+  DECLARE_CCTK_PARAMETERS;
+
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> eps_(cctkGH, eps);
+
+  const Loop::GridDescBase grid(cctkGH);
+
+  grid.loop_bnd<1, 1, 1>(grid.nghostzones,
+                         [&](const Loop::PointDesc &p) { eps_(p.I) = 0; });
+}
+
+} // namespace DGWaveToy
---
+++ DGWaveToy/src/rhs.cxx
@@ -0,0 +1,114 @@
+#include <dg.hxx>
+#include <loop.hxx>
+#include <vect.hxx>
+
+#include <cctk.h>
+#include <cctk_Arguments_Checked.h>
+#include <cctk_Parameters.h>
+
+#include <cassert>
+#include <cmath>
+
+namespace DGWaveToy {
+using namespace DG;
+using namespace std;
+
+using Loop::dim;
+
+extern "C" void DGWaveToy_RHS(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_DGWaveToy_RHS;
+  DECLARE_CCTK_PARAMETERS;
+
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> u_(cctkGH, u);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> ft_(cctkGH, ft);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fx_(cctkGH, fx);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fy_(cctkGH, fy);
+  const Loop::GF3D<const CCTK_REAL, 1, 1, 1> fz_(cctkGH, fz);
+
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> u_rhs_(cctkGH, u_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> ft_rhs_(cctkGH, ft_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fx_rhs_(cctkGH, fx_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fy_rhs_(cctkGH, fy_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fz_rhs_(cctkGH, fz_rhs);
+
+  const Loop::GridDescBase grid(cctkGH);
+
+  grid.loop_all<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+    u_rhs_(p.I) = ft_(p.I);
+  });
+
+  switch (dg_npoints) {
+
+  case 2: {
+    constexpr int N = 2;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dft = get_derivs<N>(grid, ft_, p);
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      ft_rhs_(p.I) = dfx[0] + dfy[1] + dfz[2];
+      fx_rhs_(p.I) = dft[0];
+      fy_rhs_(p.I) = dft[1];
+      fz_rhs_(p.I) = dft[2];
+    });
+    break;
+  }
+
+  case 4: {
+    constexpr int N = 4;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dft = get_derivs<N>(grid, ft_, p);
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      ft_rhs_(p.I) = dfx[0] + dfy[1] + dfz[2];
+      fx_rhs_(p.I) = dft[0];
+      fy_rhs_(p.I) = dft[1];
+      fz_rhs_(p.I) = dft[2];
+    });
+    break;
+  }
+
+  case 8: {
+    constexpr int N = 8;
+    grid.loop_int<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+      const vect<CCTK_REAL, dim> dft = get_derivs<N>(grid, ft_, p);
+      const vect<CCTK_REAL, dim> dfx = get_derivs<N>(grid, fx_, p);
+      const vect<CCTK_REAL, dim> dfy = get_derivs<N>(grid, fy_, p);
+      const vect<CCTK_REAL, dim> dfz = get_derivs<N>(grid, fz_, p);
+
+      ft_rhs_(p.I) = dfx[0] + dfy[1] + dfz[2];
+      fx_rhs_(p.I) = dft[0];
+      fy_rhs_(p.I) = dft[1];
+      fz_rhs_(p.I) = dft[2];
+    });
+    break;
+  }
+
+  default:
+    assert(0);
+  }
+}
+
+extern "C" void DGWaveToy_RHSBoundaries(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_ARGUMENTS_DGWaveToy_RHSBoundaries;
+  DECLARE_CCTK_PARAMETERS;
+
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> ft_rhs_(cctkGH, ft_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fx_rhs_(cctkGH, fx_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fy_rhs_(cctkGH, fy_rhs);
+  const Loop::GF3D<CCTK_REAL, 1, 1, 1> fz_rhs_(cctkGH, fz_rhs);
+
+  const Loop::GridDescBase grid(cctkGH);
+
+  grid.loop_bnd<1, 1, 1>(grid.nghostzones, [&](const Loop::PointDesc &p) {
+    ft_rhs_(p.I) = 0;
+    fx_rhs_(p.I) = 0;
+    fy_rhs_(p.I) = 0;
+    fz_rhs_(p.I) = 0;
+  });
+}
+
+} // namespace DGWaveToy
