07197969c3688d4e595742cea996e46be1a2d5c8
Samuel Cupp <scupp1@my.apsu.edu>
2020-10-21 07:21:38 -0500

    TestArrayGroup: cosmetic clang-format changes

--- TestArrayGroup/src/TestArray.cxx
+++ TestArrayGroup/src/TestArray.cxx
@@ -26,13 +26,13 @@ extern "C" void TestArrayGroup_Initialize(cGH *cctkGH) {
   int nmax = 4;
 
   // Initialize test array
-  for(int n=0; n<nmax; n++) {
-    for(int j=0; j<jmax; j++) {
-      for(int i=0; i<imax; i++) {
-        int index = i + j*imax + n*imax*jmax;
-        test1[index] = 1+i*j*n;
-        test2[index] = 1+7*i*j*n;
-        test3[index] = 1+13*i*j*n;
+  for (int n = 0; n < nmax; n++) {
+    for (int j = 0; j < jmax; j++) {
+      for (int i = 0; i < imax; i++) {
+        int index = i + j * imax + n * imax * jmax;
+        test1[index] = 1 + i * j * n;
+        test2[index] = 1 + 7 * i * j * n;
+        test3[index] = 1 + 13 * i * j * n;
       }
     }
   }
@@ -46,18 +46,18 @@ extern "C" void TestArrayGroup_Initialize(cGH *cctkGH) {
   ierr = CCTK_GrouplshGI(cctkGH, group.dim, lsh.data(), gi);
   assert(!ierr);
 
- for(int k=0; k < lsh[2]; k++) {
-   for(int j=0; j < lsh[1]; j++) {
-     for(int i=0; i < lsh[0]; i++) {
-       int index = CCTK_GFINDEX3D(cctkGH, i, j, k);
-       test_gf[index] = i*j*k;
-     }
-   }
- }
+  for (int k = 0; k < lsh[2]; k++) {
+    for (int j = 0; j < lsh[1]; j++) {
+      for (int i = 0; i < lsh[0]; i++) {
+        int index = CCTK_GFINDEX3D(cctkGH, i, j, k);
+        test_gf[index] = i * j * k;
+      }
+    }
+  }
 
   *test_scalar = 1;
 
- return;
+  return;
 }
 
 extern "C" void TestArrayGroup_Compare(cGH *cctkGH) {
@@ -68,50 +68,56 @@ extern "C" void TestArrayGroup_Compare(cGH *cctkGH) {
   int jmax = 6;
   int nmax = 4;
 
-  int error_count[3] = {0,0,0};
+  int error_count[3] = {0, 0, 0};
   std::string vname;
 
-  for(int n=0; n<nmax; n++) {
+  for (int n = 0; n < nmax; n++) {
     vname = "TestArrayGroup::test1[" + std::to_string(n) + "]";
-    CCTK_REAL *var1 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH,0,vname.c_str());
+    CCTK_REAL *var1 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH, 0, vname.c_str());
 
     vname = "TestArrayGroup::test2[" + std::to_string(n) + "]";
-    CCTK_REAL *var2 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH,0,vname.c_str());
+    CCTK_REAL *var2 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH, 0, vname.c_str());
 
     vname = "TestArrayGroup::test3[" + std::to_string(n) + "]";
-    CCTK_REAL *var3 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH,0,vname.c_str());
-    for(int j=0; j<jmax; j++) {
-      for(int i=0; i<imax; i++) {
-        int index = i + j*imax;
-        if (var1[index] != 1+i*j*n) error_count[0] += 1;
-        if (var2[index] != 1+7*i*j*n) error_count[1] += 1;
-        if (var3[index] != 1+13*i*j*n) error_count[2] += 1;
+    CCTK_REAL *var3 = (CCTK_REAL *)CCTK_VarDataPtr(cctkGH, 0, vname.c_str());
+    for (int j = 0; j < jmax; j++) {
+      for (int i = 0; i < imax; i++) {
+        int index = i + j * imax;
+        if (var1[index] != 1 + i * j * n)
+          error_count[0] += 1;
+        if (var2[index] != 1 + 7 * i * j * n)
+          error_count[1] += 1;
+        if (var3[index] != 1 + 13 * i * j * n)
+          error_count[2] += 1;
       }
     }
   }
 
   if (error_count[0] > 0) {
-    int size = nmax*jmax*imax;
+    int size = nmax * jmax * imax;
     std::string msg;
-    msg = "TestArrayGroup: grid array test1 failed in " + std::to_string(error_count[0]) + 
-          " of " + std::to_string(size) + " elements.";
+    msg = "TestArrayGroup: grid array test1 failed in " +
+          std::to_string(error_count[0]) + " of " + std::to_string(size) +
+          " elements.";
     CCTK_ERROR(msg.c_str());
   }
 
   if (error_count[1] > 0) {
-    int size = nmax*jmax*imax;
+    int size = nmax * jmax * imax;
     std::string msg;
-    msg = "TestArrayGroup: grid array test2 failed in " + std::to_string(error_count[1]) + 
-          " of " + std::to_string(size) + " elements.";
+    msg = "TestArrayGroup: grid array test2 failed in " +
+          std::to_string(error_count[1]) + " of " + std::to_string(size) +
+          " elements.";
     CCTK_ERROR(msg.c_str());
   }
 
   if (error_count[2] > 0) {
-    int size = nmax*jmax*imax;
+    int size = nmax * jmax * imax;
     std::string msg;
-    msg = "TestArrayGroup: grid array test3 failed in " + std::to_string(error_count[2]) + 
-          " of " + std::to_string(size) + " elements.";
+    msg = "TestArrayGroup: grid array test3 failed in " +
+          std::to_string(error_count[2]) + " of " + std::to_string(size) +
+          " elements.";
     CCTK_ERROR(msg.c_str());
   }
- return;
+  return;
 }
