cfcec41eb637559595c2f22c1c9c90ddc1bd2c49
Erik Schnetter <schnetter@gmail.com>
2021-02-25 18:41:18 -0500

    CarpetX: More self checks when converting grid shapes

--- CarpetX/src/schedule.cxx
+++ CarpetX/src/schedule.cxx
@@ -104,30 +104,44 @@ bool CarpetX_poison_undefined_values = true;
 GridDescBase::GridDescBase() {}
 
 GridDescBase::GridDescBase(const cGH *restrict cctkGH) {
-  for (int d = 0; d < dim; ++d)
-    gsh[d] = cctkGH->cctk_gsh[d];
   for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_gsh[d] != undefined);
+    gsh[d] = cctkGH->cctk_gsh[d];
+  }
+  for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_lbnd[d] != undefined);
+    assert(cctkGH->cctk_ubnd[d] != undefined);
     lbnd[d] = cctkGH->cctk_lbnd[d];
     ubnd[d] = cctkGH->cctk_ubnd[d];
   }
   for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_tile_min[d] != undefined);
+    assert(cctkGH->cctk_tile_max[d] != undefined);
     tmin[d] = cctkGH->cctk_tile_min[d];
     tmax[d] = cctkGH->cctk_tile_max[d];
   }
-  for (int d = 0; d < dim; ++d)
+  for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_lsh[d] != undefined);
     lsh[d] = cctkGH->cctk_lsh[d];
-  for (int d = 0; d < dim; ++d)
+  }
+  for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_ash[d] != undefined);
     ash[d] = cctkGH->cctk_ash[d];
-  for (int d = 0; d < dim; ++d)
-    for (int f = 0; f < 2; ++f)
+  }
+  for (int d = 0; d < dim; ++d) {
+    for (int f = 0; f < 2; ++f) {
+      assert(cctkGH->cctk_bbox[2 * d + f] != undefined);
       bbox[2 * d + f] = cctkGH->cctk_bbox[2 * d + f];
-  for (int d = 0; d < dim; ++d)
+    }
+  }
+  for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_nghostzones[d] != undefined);
     nghostzones[d] = cctkGH->cctk_nghostzones[d];
+  }
 
   // Check whether we are in local mode
-  assert(cctkGH->cctk_bbox[0] != CarpetX::undefined);
-  int thread_num = omp_get_thread_num();
   if (omp_in_parallel()) {
+    int thread_num = omp_get_thread_num();
     const cGH *restrict threadGH =
         &CarpetX::thread_local_info.at(thread_num)->cctkGH;
     // Check whether this is the correct cGH structure
@@ -135,6 +149,9 @@ GridDescBase::GridDescBase(const cGH *restrict cctkGH) {
   }
 
   for (int d = 0; d < dim; ++d) {
+    assert(cctkGH->cctk_levfac[d] != undefined);
+    assert(cctkGH->cctk_levoff[d] != undefined);
+    assert(cctkGH->cctk_levoffdenom[d] != 0);
     dx[d] = cctkGH->cctk_delta_space[d] / cctkGH->cctk_levfac[d];
     x0[d] = cctkGH->cctk_origin_space[d] +
             dx[d] * cctkGH->cctk_levoff[d] / cctkGH->cctk_levoffdenom[d];
@@ -151,6 +168,9 @@ GridDesc::GridDesc(const GHExt::LevelData &leveldata, const MFPointer &mfp) {
   const amrex::Box &vbx = mfp.validbox(); // interior region (without ghosts)
   const amrex::Box &gbx = mfp.growntilebox(); // current region (with ghosts)
   const amrex::Box &domain = ghext->amrcore->Geom(leveldata.level).Domain();
+
+  for (int d = 0; d < dim; ++d)
+    assert(domain.type(d) == amrex::IndexType::CELL);
 
   // The number of ghostzones in each direction
   for (int d = 0; d < dim; ++d)
