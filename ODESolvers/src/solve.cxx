65b0e5f7635a3016ce02eef20f57d8f4a24caac2
Erik Schnetter <schnetter@gmail.com>
2021-05-02 16:33:45 -0400

    ODESovlers: Add helper function make_array

--- ODESolvers/src/solve.cxx
+++ ODESolvers/src/solve.cxx
@@ -8,6 +8,7 @@
 
 #include <AMReX_MultiFab.H>
 
+#include <array>
 #include <cassert>
 #include <cmath>
 #include <functional>
@@ -20,6 +21,38 @@
 
 namespace ODESolver {
 using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Taken from <https://en.cppreference.com/w/cpp/experimental/make_array>
+namespace details {
+template <class> struct is_ref_wrapper : std::false_type {};
+template <class T>
+struct is_ref_wrapper<std::reference_wrapper<T> > : std::true_type {};
+
+template <class T>
+using not_ref_wrapper = std::negation<is_ref_wrapper<std::decay_t<T> > >;
+
+template <class D, class...> struct return_type_helper { using type = D; };
+template <class... Types>
+struct return_type_helper<void, Types...> : std::common_type<Types...> {
+  static_assert(std::conjunction_v<not_ref_wrapper<Types>...>,
+                "Types cannot contain reference_wrappers when D is void");
+};
+
+template <class D, class... Types>
+using return_type = std::array<typename return_type_helper<D, Types...>::type,
+                               sizeof...(Types)>;
+} // namespace details
+
+template <class D = void, class... Types>
+constexpr details::return_type<D, Types...> make_array(Types &&...t) {
+  // Don't warn when converting int to double
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnarrowing"
+  return {std::forward<Types>(t)...};
+#pragma GCC diagnostic pop
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
