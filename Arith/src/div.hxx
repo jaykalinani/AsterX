6be79011d604331a81ac8cd738bb3eebe880aab7
Erik Schnetter <schnetter@gmail.com>
2021-05-07 20:15:34 -0400

    Arith: Add alignment functions

--- Arith/src/div.hxx
+++ Arith/src/div.hxx
@@ -12,8 +12,27 @@ template <typename T> constexpr ARITH_INLINE T div_floor(T x, T y) {
   return (x >= 0 ? x : x - y + 1) / y;
 }
 
+template <typename T> constexpr ARITH_INLINE T align_floor(T x, T y) {
+  return div_floor(x, y) * y;
+}
+
 template <typename T> constexpr ARITH_INLINE T mod_floor(T x, T y) {
-  return x - div_floor(x, y) * y;
+  return x - align_floor(x, y);
+}
+
+template <typename T> constexpr ARITH_INLINE T div_ceil(T x, T y) {
+  // C++ division truncates; we want to round towards +infinity instead
+  x = y < 0 ? -x : x;
+  y = y < 0 ? -y : y;
+  return (x >= 0 ? x + y - 1 : x) / y;
+}
+
+template <typename T> constexpr ARITH_INLINE T align_ceil(T x, T y) {
+  return div_ceil(x, y) * y;
+}
+
+template <typename T> constexpr ARITH_INLINE T mod_ceil(T x, T y) {
+  return x - align_ceil(x, y);
 }
 
 } // namespace Arith
