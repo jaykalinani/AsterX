d10af7db5668e801a57ca5c2a561f312ad198ea6
Erik Schnetter <schnetter@gmail.com>
2021-05-30 11:20:08 -0400

    Z4c: Correct mixed second derivative operator

--- Z4c/src/test.cxx
+++ Z4c/src/test.cxx
@@ -1,6 +1,9 @@
 #include "derivs.hxx"
 #include "physics.hxx"
 
+#include <defs.hxx>
+#include <simd.hxx>
+
 #include <fixmath.hxx> // include this before <cctk.h>
 #include <cctk.h>
 
@@ -8,6 +11,7 @@
 #include <array>
 #include <cassert>
 #include <cmath>
+#include <iostream>
 #include <random>
 
 namespace Z4c {
@@ -17,8 +21,6 @@ using namespace std;
 
 extern "C" void Z4c_Test(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
-
-#ifdef __CUDACC_
 
   // Test tensors
 
@@ -30,21 +32,22 @@ extern "C" void Z4c_Test(CCTK_ARGUMENTS) {
     for (int a = 0; a < 3; ++a)
       for (int b = 0; b < 3; ++b)
         arr[a][b] = rand10();
-    return smat<double, DN, DN>(
+    return smat<double, 3, DN, DN>(
         [&](int a, int b) { return arr[min(a, b)][max(a, b)]; });
   }};
 
-  const smat<double, DN, DN> Z([&](int a, int b) { return double(0); });
-  const smat<double, DN, DN> I([&](int a, int b) { return double(a == b); });
+  const smat<double, 3, DN, DN> Z([&](int a, int b) { return double(0); });
+  const smat<double, 3, DN, DN> I([&](int a, int b) { return double(a == b); });
   assert(I != Z);
-  const smat<double, UP, UP> Zup([&](int a, int b) { return double(0); });
-  const smat<double, UP, UP> Iup([&](int a, int b) { return double(a == b); });
+  const smat<double, 3, UP, UP> Zup([&](int a, int b) { return double(0); });
+  const smat<double, 3, UP, UP> Iup(
+      [&](int a, int b) { return double(a == b); });
   assert(Iup != Zup);
 
   for (int n = 0; n < 100; ++n) {
-    const smat<double, DN, DN> A = randmat10();
-    const smat<double, DN, DN> B = randmat10();
-    const smat<double, DN, DN> C = randmat10();
+    const smat<double, 3, DN, DN> A = randmat10();
+    const smat<double, 3, DN, DN> B = randmat10();
+    const smat<double, 3, DN, DN> C = randmat10();
     const double a = rand10();
     const double b = rand10();
 
@@ -70,109 +73,170 @@ extern "C" void Z4c_Test(CCTK_ARGUMENTS) {
     // DNUP  assert(mul(Z, A) == Z);
     // DNUP  assert(mul(A, Z) == Z);
 
-    assert(Z.det() == 0);
-    assert(I.det() == 1);
-    assert((a * A).det() == pow3(a) * A.det());
+    assert(calc_det(Z) == 0);
+    assert(calc_det(I) == 1);
+    assert(calc_det(a * A) == pown(a, 3) * calc_det(A));
 
-    assert(Z.inv(1) == Zup);
-    assert(I.inv(1) == Iup);
+    assert(calc_inv(Z, 1.0) == Zup);
+    assert(calc_inv(I, 1.0) == Iup);
 
     // DNUP assert(mul(A.inv(1), A) == A.det() * Iup);
     // DNUP assert(mul(A, A.inv(1)) == A.det() * Iup);
 
-    assert((a * A).inv(1) == pow2(a) * A.inv(1));
+    assert(calc_inv(a * A, 1.0) == pow2(a) * calc_inv(A, 1.0));
   }
 
   // Test derivatives
 
   static_assert(deriv_order % 2 == 0, "");
   constexpr int required_ghosts = deriv_order / 2 + 1;
+  constexpr int fences = 3;
+  constexpr int vsize = tuple_size_v<simd<double> >;
   const double eps = 1.0e-12;
 
   // deriv
-  for (int order = 0; order <= deriv_order; ++order) {
-    // CCTK_VINFO("Testing deriv order=%d", order);
-    array<double, 2 * required_ghosts + 7> arr;
-    for (size_t i = 0; i < arr.size(); ++i)
-      arr[i] = NAN;
-    double *const var = &arr[arr.size() / 2];
-    for (int i = -deriv_order / 2; i <= deriv_order / 2; ++i)
-      var[i] = pown(i, order);
-    const double expected = order == 1 ? 1 : 0;
-    const double found = deriv1d(var, 1, 1.0);
-    assert(fabs(found - expected) <= eps);
+  for (int npoints = 1; npoints <= vsize; ++npoints) {
+    for (int order = 0; order <= deriv_order; ++order) {
+      // CCTK_VINFO("Testing deriv order=%d", order);
+      array<double, 2 * (fences + required_ghosts) + vsize> arr;
+      for (size_t i = 0; i < arr.size(); ++i)
+        arr[i] = NAN;
+      double *const var = &arr[fences + required_ghosts];
+      for (int i = -deriv_order / 2; i < vsize + deriv_order / 2; ++i)
+        var[i] = pown(i, order);
+      const simd<double> expected =
+          order == 0 ? 0 : order * pown(iota<simd<double> >(), order - 1);
+      const simdl<double> mask = mask_for_loop_tail<simdl<double> >(0, npoints);
+      const simd<double> found = deriv1d(mask, var, 1, 1.0);
+      if (!(all(fabs(found - expected) <= eps || !mask)))
+        cout << "deriv:\n"
+             << "  npoints: " << npoints << "\n"
+             << "  order: " << order << "\n"
+             << "  expected: " << expected << "\n"
+             << "  found: " << found << "\n";
+      assert(all(fabs(found - expected) <= eps || !mask));
+    }
   }
 
   // deriv (upwind)
-  for (int order = 0; order <= deriv_order; ++order) {
-    for (int sign = 0; sign <= 1; ++sign) {
-      // CCTK_VINFO("Testing deriv (upwind) order=%d sign=%d", order, sign);
-      array<double, 2 * required_ghosts + 7> arr;
-      for (size_t i = 0; i < arr.size(); ++i)
-        arr[i] = NAN;
-      double *const var = &arr[arr.size() / 2];
-      if (sign)
-        for (int i = -deriv_order / 2 - 1; i <= deriv_order / 2 - 1; ++i)
+  for (int npoints = 1; npoints <= vsize; ++npoints) {
+    for (int order = 0; order <= deriv_order; ++order) {
+      for (int sign = 0; sign <= 1; ++sign) {
+        // CCTK_VINFO("Testing deriv (upwind) order=%d sign=%d", order, sign);
+        array<double, 2 * (fences + required_ghosts) + vsize> arr;
+        for (size_t i = 0; i < arr.size(); ++i)
+          arr[i] = NAN;
+        double *const var = &arr[fences + required_ghosts];
+        for (int i = -deriv_order / 2 - 1; i < vsize + deriv_order / 2 + 1; ++i)
           var[i] = pown(i, order);
-      else
-        for (int i = -deriv_order / 2 + 1; i <= deriv_order / 2 + 1; ++i)
-          var[i] = pown(i, order);
-      const double expected = order == 1 ? 1 : 0;
-      const double found = deriv1d_upwind(var, 1, sign, 1.0);
-      assert(fabs(found - expected) <= eps);
+        const simd<double> vel = sign ? -1 : +1;
+        const simd<double> expected =
+            vel *
+            (order == 0 ? 0 : order * pown(iota<simd<double> >(), order - 1));
+        const simdl<double> mask =
+            mask_for_loop_tail<simdl<double> >(0, npoints);
+        const simd<double> found = deriv1d_upwind(mask, var, 1, vel, 1.0);
+        if (!(all(fabs(found - expected) <= eps || !mask)))
+          cout << "deriv_upwind:\n"
+               << "  npoints: " << npoints << "\n"
+               << "  order: " << order << "\n"
+               << "  sign: " << sign << "\n"
+               << "  expected: " << expected << "\n"
+               << "  found: " << found << "\n";
+        assert(all(fabs(found - expected) <= eps || !mask));
+      }
     }
   }
 
   // deriv2
-  for (int order = 0; order <= deriv_order; ++order) {
-    // CCTK_VINFO("Testing deriv2 order=%d", order);
-    array<double, 2 * required_ghosts + 7> arr;
-    for (size_t i = 0; i < arr.size(); ++i)
-      arr[i] = NAN;
-    double *const var = &arr[arr.size() / 2];
-    for (int i = -deriv_order / 2; i <= deriv_order / 2; ++i)
-      var[i] = pown(i, order);
-    const double expected = order == 2 ? 2 : 0;
-    const double found = deriv2_1d(var, 1, 1.0);
-    assert(fabs(found - expected) <= eps);
+  for (int npoints = 1; npoints <= vsize; ++npoints) {
+    for (int order = 0; order <= deriv_order; ++order) {
+      // CCTK_VINFO("Testing deriv2 order=%d", order);
+      array<double, 2 * (fences + required_ghosts) + vsize> arr;
+      for (size_t i = 0; i < arr.size(); ++i)
+        arr[i] = NAN;
+      double *const var = &arr[fences + required_ghosts];
+      for (int i = -deriv_order / 2; i < vsize + deriv_order / 2; ++i)
+        var[i] = pown(i, order);
+      const simd<double> expected =
+          order < 2
+              ? 0
+              : order * (order - 1) * pown(iota<simd<double> >(), order - 2);
+      const simdl<double> mask = mask_for_loop_tail<simdl<double> >(0, npoints);
+      const simd<double> found = deriv2_1d(mask, var, 1, 1.0);
+      if (!(all(fabs(found - expected) <= eps || !mask)))
+        cout << "deriv_upwind:\n"
+             << "  npoints: " << npoints << "\n"
+             << "  order: " << order << "\n"
+             << "  expected: " << expected << "\n"
+             << "  found: " << found << "\n";
+      assert(all(fabs(found - expected) <= eps || !mask));
+    }
   }
 
   // deriv2 (mixed)
-  for (int orderj = 0; orderj <= deriv_order; ++orderj) {
-    for (int orderi = 0; orderi <= deriv_order; ++orderi) {
-      // CCTK_VINFO("Testing deriv2 (mixed) order=%d,%d", orderi, orderj);
-      array<array<double, 2 * required_ghosts + 7>, 2 * required_ghosts + 7>
-          arr;
-      for (size_t j = 0; j < arr.size(); ++j)
-        for (size_t i = 0; i < arr[j].size(); ++i)
-          arr[j][i] = NAN;
-      const int di = 1;
-      const int dj = arr[0].size();
-      double *const var = &arr[arr.size() / 2][arr[0].size() / 2];
-      for (int j = -deriv_order / 2; j <= deriv_order / 2; ++j)
-        for (int i = -deriv_order / 2; i <= deriv_order / 2; ++i)
-          var[j * dj + i * di] = pown(i, orderi) * pown(j, orderj);
-      const double expected = orderi == 1 && orderj == 1 ? 1 : 0;
-      const double found = deriv2_2d(var, di, dj, 1.0, 1.0);
-      assert(fabs(found - expected) <= eps);
+  for (int npoints = 1; npoints <= vsize; ++npoints) {
+    for (int orderj = 0; orderj <= deriv_order; ++orderj) {
+      for (int orderi = 0; orderi <= deriv_order; ++orderi) {
+        // CCTK_VINFO("Testing deriv2 (mixed) order=%d,%d", orderi, orderj);
+        array<array<double, 2 * (fences + required_ghosts) + vsize>,
+              2 * (fences + required_ghosts) + 1>
+            arr;
+        for (size_t j = 0; j < arr.size(); ++j)
+          for (size_t i = 0; i < arr[0].size(); ++i)
+            arr[j][i] = NAN;
+        const int di = 1;
+        const int dj = arr[0].size();
+        double *const var =
+            &arr[fences + required_ghosts][fences + required_ghosts];
+        for (int j = -deriv_order / 2; j < 1 + deriv_order / 2; ++j)
+          for (int i = -deriv_order / 2; i < vsize + deriv_order / 2; ++i)
+            var[j * dj + i * di] = pown(i, orderi) * pown(j, orderj);
+        const simd<double> expected =
+            (orderi == 0 ? 0
+                         : orderi * pown(iota<simd<double> >(), orderi - 1)) *
+            (orderj == 0 ? 0 : orderj * pown(0.0, orderj - 1));
+        const simdl<double> mask =
+            mask_for_loop_tail<simdl<double> >(0, npoints);
+        const simd<double> found =
+            deriv2_2d(npoints, mask, var, di, dj, 1.0, 1.0);
+        if (!(all(fabs(found - expected) <= eps || !mask)))
+          cout << "deriv2_mixed:\n"
+               << "  npoints: " << npoints << "\n"
+               << "  orderi: " << orderi << "\n"
+               << "  orderj: " << orderj << "\n"
+               << "  expected: " << expected << "\n"
+               << "  found: " << found << "\n";
+        assert(all(fabs(found - expected) <= eps || !mask));
+      }
     }
   }
 
   // deriv (dissipation)
-  for (int order = 0; order <= deriv_order + 2; ++order) {
-    // CCTK_VINFO("Testing deriv (dissipation) order=%d", order);
-    array<double, 2 * required_ghosts + 7> arr;
-    for (size_t i = 0; i < arr.size(); ++i)
-      arr[i] = NAN;
-    double *const var = &arr[arr.size() / 2];
-    for (int i = -deriv_order / 2 - 1; i <= deriv_order / 2 + 1; ++i)
-      var[i] = pown(i, order);
-    const double expected = order == deriv_order + 2 ? factorial(order) : 0;
-    const double found = deriv1d_diss(var, 1, 1.0);
-    assert(fabs(found - expected) <= eps);
+  for (int npoints = 1; npoints <= vsize; ++npoints) {
+    for (int order = 0; order <= deriv_order + 2; ++order) {
+      // CCTK_VINFO("Testing deriv (dissipation) order=%d", order);
+      array<double, 2 * (fences + required_ghosts) + vsize> arr;
+      for (size_t i = 0; i < arr.size(); ++i)
+        arr[i] = NAN;
+      double *const var = &arr[fences + required_ghosts];
+      for (int i = -deriv_order / 2 - 1; i < vsize + deriv_order / 2 + 1; ++i)
+        var[i] = pown(i, order);
+      const simd<double> expected =
+          order < deriv_order + 2
+              ? 0
+              : factorial(order) * pown(iota<simd<double> >(), 0);
+      const simdl<double> mask = mask_for_loop_tail<simdl<double> >(0, npoints);
+      const simd<double> found = deriv1d_diss(mask, var, 1, 1.0);
+      if (!(all(fabs(found - expected) <= eps || !mask)))
+        cout << "deriv_diss:\n"
+             << "  npoints: " << npoints << "\n"
+             << "  order: " << order << "\n"
+             << "  expected: " << expected << "\n"
+             << "  found: " << found << "\n";
+      assert(all(fabs(found - expected) <= eps || !mask));
+    }
   }
-
-#endif
 }
 
 } // namespace Z4c
