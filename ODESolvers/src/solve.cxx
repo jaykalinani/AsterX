ce73459ccf060212e477d2d63942bb71262713b9
Erik Schnetter <schnetter@gmail.com>
2021-12-02 18:25:38 -0500

    ODESolvers: Fuse GPU kernels

--- ODESolvers/src/solve.cxx
+++ ODESolvers/src/solve.cxx
@@ -191,80 +191,80 @@ void statecomp_t::lincomb(const statecomp_t &dst, const CCTK_REAL scale,
       }
       const ptrdiff_t nstride = dstvar.nstride;
 
-      for (size_t c = 0; c < ncomp; ++c) {
-        CCTK_REAL *restrict const dstptr = dstvar.dataPtr() + c * nstride;
-        array<const CCTK_REAL *restrict, N> srcptrs;
-        for (size_t n = 0; n < N; ++n)
-          srcptrs[n] = srcvars[n].dataPtr() + c * nstride;
+      CCTK_REAL *restrict const dstptr = dstvar.dataPtr();
+      array<const CCTK_REAL *restrict, N> srcptrs;
+      for (size_t n = 0; n < N; ++n)
+        srcptrs[n] = srcvars[n].dataPtr();
+      const ptrdiff_t npoints = nstride * ncomp;
 
 #ifndef AMREX_USE_GPU
-        // CPU
+      // CPU
 
-        if (!read_dst) {
+      if (!read_dst) {
 
-          auto task = [=]() {
+        auto task = [=]() {
 #pragma omp simd
-            for (ptrdiff_t i = 0; i < nstride; ++i) {
-              CCTK_REAL accum = 0;
-              for (size_t n = 0; n < N; ++n)
-                accum += factors[n] * srcptrs[n][i];
-              dstptr[i] = accum;
-            }
-          };
-          tasks.emplace_back(move(task));
+          for (ptrdiff_t i = 0; i < npoints; ++i) {
+            CCTK_REAL accum = 0;
+            for (size_t n = 0; n < N; ++n)
+              accum += factors[n] * srcptrs[n][i];
+            dstptr[i] = accum;
+          }
+        };
+        tasks.emplace_back(move(task));
 
-        } else {
+      } else {
 
-          auto task = [=]() {
+        auto task = [=]() {
 #pragma omp simd
-            for (ptrdiff_t i = 0; i < nstride; ++i) {
-              CCTK_REAL accum = scale * dstptr[i];
-              for (size_t n = 0; n < N; ++n)
-                accum += factors[n] * srcptrs[n][i];
-              dstptr[i] = accum;
-            }
-          };
-          tasks.emplace_back(move(task));
-        }
+          for (ptrdiff_t i = 0; i < npoints; ++i) {
+            CCTK_REAL accum = scale * dstptr[i];
+            for (size_t n = 0; n < N; ++n)
+              accum += factors[n] * srcptrs[n][i];
+            dstptr[i] = accum;
+          }
+        };
+        tasks.emplace_back(move(task));
+      }
 
 #else
-        // GPU
+      // GPU
 
-        const CCTK_REAL scale1 = scale;
-        const amrex::Box box(
-            amrex::IntVect(0, 0, 0), amrex::IntVect(nstride - 1, 0, 0),
-            amrex::IntVect(amrex::IndexType::CELL, amrex::IndexType::CELL,
-                           amrex::IndexType::CELL));
+      const CCTK_REAL scale1 = scale;
+      assert(npoints < INT_MAX);
+      const amrex::Box box(
+          amrex::IntVect(0, 0, 0), amrex::IntVect(npoints - 1, 0, 0),
+          amrex::IntVect(amrex::IndexType::CELL, amrex::IndexType::CELL,
+                         amrex::IndexType::CELL));
 
-        if (!read_dst) {
+      if (!read_dst) {
 
-          amrex::launch(box, [=] CCTK_DEVICE(const amrex::Box &box)
-                                 CCTK_ATTRIBUTE_ALWAYS_INLINE {
-                                   const int i = box.smallEnd()[0];
-                                   // const int j = box.smallEnd()[1];
-                                   // const int k = box.smallEnd()[2];
-                                   CCTK_REAL accum = 0;
-                                   for (size_t n = 0; n < N; ++n)
-                                     accum += factors[n] * srcptrs[n][i];
-                                   dstptr[i] = accum;
-                                 });
+        amrex::launch(box, [=] CCTK_DEVICE(const amrex::Box &box)
+                               CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                 const int i = box.smallEnd()[0];
+                                 // const int j = box.smallEnd()[1];
+                                 // const int k = box.smallEnd()[2];
+                                 CCTK_REAL accum = 0;
+                                 for (size_t n = 0; n < N; ++n)
+                                   accum += factors[n] * srcptrs[n][i];
+                                 dstptr[i] = accum;
+                               });
 
-        } else {
+      } else {
 
-          amrex::launch(box, [=] CCTK_DEVICE(const amrex::Box &box)
-                                 CCTK_ATTRIBUTE_ALWAYS_INLINE {
-                                   const int i = box.smallEnd()[0];
-                                   // const int j = box.smallEnd()[1];
-                                   // const int k = box.smallEnd()[2];
-                                   CCTK_REAL accum = scale1 * dstptr[i];
-                                   for (size_t n = 0; n < N; ++n)
-                                     accum += factors[n] * srcptrs[n][i];
-                                   dstptr[i] = accum;
-                                 });
-        }
+        amrex::launch(box, [=] CCTK_DEVICE(const amrex::Box &box)
+                               CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                 const int i = box.smallEnd()[0];
+                                 // const int j = box.smallEnd()[1];
+                                 // const int k = box.smallEnd()[2];
+                                 CCTK_REAL accum = scale1 * dstptr[i];
+                                 for (size_t n = 0; n < N; ++n)
+                                   accum += factors[n] * srcptrs[n][i];
+                                 dstptr[i] = accum;
+                               });
+      }
 
 #endif
-      }
     }
   }
 
