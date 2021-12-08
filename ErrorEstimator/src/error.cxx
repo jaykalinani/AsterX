4dc5b8c37fd969c2791d13133f5ddc6a15b67ead
Erik Schnetter <schnetter@gmail.com>
2021-12-08 16:00:12 -0500

    ErrorEstimator: Run on GPUs

--- ErrorEstimator/src/error.cxx
+++ ErrorEstimator/src/error.cxx
@@ -1,10 +1,11 @@
-#include <loop.hxx>
+#include <loop_device.hxx>
 
 #include <cctk.h>
 #include <cctk_Arguments_Checked.h>
 #include <cctk_Parameters.h>
 
 #include <algorithm>
+#include <array>
 #include <cassert>
 #include <cmath>
 
@@ -12,9 +13,10 @@ namespace ErrorEstimator {
 using namespace Loop;
 using namespace std;
 
-template <typename T> constexpr T pow2(const T x) { return x * x; }
+template <typename T> CCTK_DEVICE constexpr T pow2(const T x) { return x * x; }
 
-template <typename T> constexpr T fmax3(const T x, const T y, const T z) {
+template <typename T>
+CCTK_DEVICE constexpr T fmax3(const T x, const T y, const T z) {
   return fmax(fmax(x, y), z);
 }
 
@@ -43,38 +45,44 @@ extern "C" void ErrorEstimator_Estimate(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_ErrorEstimator_Estimate;
   DECLARE_CCTK_PARAMETERS;
 
+  const array<int, dim> indextype = {1, 1, 1};
+  const GF3D2layout layout(cctkGH, indextype);
+
   const CCTK_REAL dx = CCTK_DELTA_SPACE(0);
   const CCTK_REAL dy = CCTK_DELTA_SPACE(1);
   const CCTK_REAL dz = CCTK_DELTA_SPACE(2);
-
   const CCTK_REAL scalefactor = scale_by_resolution ? cbrt(dx * dy * dz) : 1;
 
-  const GF3D<CCTK_REAL, 1, 1, 1> regrid_error_(cctkGH, regrid_error);
+  const GF3D2<CCTK_REAL> regrid_error_(layout, regrid_error);
 
-  typedef CCTK_REAL radius_t(CCTK_REAL x, CCTK_REAL y, CCTK_REAL z);
-  radius_t *const radius = [](const char *const sh) {
-    if (CCTK_EQUALS(sh, "sphere"))
-      return (radius_t *)radius_sphere;
-    else if (CCTK_EQUALS(sh, "cube"))
-      return (radius_t *)radius_cube;
+  const shape_t shape = [&]() {
+    if (CCTK_EQUALS(region_shape, "sphere"))
+      return shape_t::sphere;
+    if (CCTK_EQUALS(region_shape, "cube"))
+      return shape_t::cube;
     abort();
-  }(region_shape);
+  }();
 
-  loop_int<1, 1, 1>(cctkGH, [&](const PointDesc &p) {
-    CCTK_REAL maxerr = 0;
-    // Sample the neighbourhood of this point
-    for (int k = -1; k <= +1; ++k) {
-      for (int j = -1; j <= +1; ++j) {
-        for (int i = -1; i <= +1; ++i) {
-          const auto r =
-              radius(p.x + i * dx / 2, p.y + j * dy / 2, p.z + k * dz / 2);
-          const auto err = scalefactor / fmax(r, epsilon);
-          maxerr = fmax(maxerr, err);
-        }
-      }
-    }
-    regrid_error_(p.I) = maxerr;
-  });
+  const GridDescBaseDevice grid(cctkGH);
+  grid.loop_int_device<1, 1, 1>(
+      grid.nghostzones,
+      [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+        const CCTK_REAL x = fabs(p.x) + fabs(dx);
+        const CCTK_REAL y = fabs(p.y) + fabs(dy);
+        const CCTK_REAL z = fabs(p.z) + fabs(dz);
+        const CCTK_REAL r = [&]() {
+          switch (shape) {
+          case shape_t::sphere:
+            return radius_sphere(x, y, z);
+          case shape_t::cube:
+            return radius_cube(x, y, z);
+          default:
+            assert(0);
+          };
+        }();
+        const CCTK_REAL err = scalefactor / fmax(r, epsilon);
+        regrid_error_(p.I) = err;
+      });
 }
 
 } // namespace ErrorEstimator
