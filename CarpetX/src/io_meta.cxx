aee9d038c0423da7991d0c6bc6e9bafae6ae9e00
Erik Schnetter <schnetter@gmail.com>
2021-12-12 00:24:24 -0500

    CarpetX: Handle meta I/O when there are no variables

--- CarpetX/src/io_meta.cxx
+++ CarpetX/src/io_meta.cxx
@@ -166,149 +166,150 @@ real_output_file_description_t::real_output_file_description_t(
       for (auto &ch : var)
         ch = std::tolower(ch);
     }
+  }
 
-    iterations = ofd.iterations;
-    if (iterations.empty())
-      CCTK_ERROR("Empty iterations");
-    for (const auto &iter : iterations)
-      if (iter < 0)
-        CCTK_VERROR("Bad iteration number %d", iter);
+  iterations = ofd.iterations;
+  if (iterations.empty())
+    CCTK_ERROR("Empty iterations");
+  for (const auto &iter : iterations)
+    if (iter < 0)
+      CCTK_VERROR("Bad iteration number %d", iter);
 
-    {
-      const int groupindex0 = CCTK_GroupIndexFromVar(variables.front().c_str());
-      assert(groupindex0 >= 0);
-      const int grouptype0 = CCTK_GroupTypeI(groupindex0);
-      switch (grouptype0) {
+  {
+    const int groupindex0 = CCTK_GroupIndexFromVar(variables.front().c_str());
+    assert(groupindex0 >= 0);
+    const int grouptype0 = CCTK_GroupTypeI(groupindex0);
+    switch (grouptype0) {
+    case CCTK_GF:
+      group_type = group_type_t::gf;
+      break;
+    case CCTK_SCALAR:
+    case CCTK_ARRAY:
+      group_type = group_type_t::array;
+      break;
+    default:
+      assert(0);
+    }
+    variable_dimensions = CCTK_GroupDimI(groupindex0);
+    for (auto &var : variables) {
+      const int groupindex = CCTK_GroupIndexFromVar(var.c_str());
+      assert(groupindex >= 0);
+      const int grouptype = CCTK_GroupTypeI(groupindex);
+      group_type_t group_type1;
+      switch (grouptype) {
       case CCTK_GF:
-        group_type = group_type_t::gf;
+        group_type1 = group_type_t::gf;
         break;
       case CCTK_SCALAR:
       case CCTK_ARRAY:
-        group_type = group_type_t::array;
+        group_type1 = group_type_t::array;
         break;
       default:
         assert(0);
       }
-      variable_dimensions = CCTK_GroupDimI(groupindex0);
-      for (auto &var : variables) {
-        const int groupindex = CCTK_GroupIndexFromVar(var.c_str());
-        assert(groupindex >= 0);
-        const int grouptype = CCTK_GroupTypeI(groupindex);
-        group_type_t group_type1;
-        switch (grouptype) {
-        case CCTK_GF:
-          group_type1 = group_type_t::gf;
-          break;
-        case CCTK_SCALAR:
-        case CCTK_ARRAY:
-          group_type1 = group_type_t::array;
-          break;
-        default:
-          assert(0);
-        }
-        if (group_type1 != group_type)
-          CCTK_VERROR("Variable \"%s\" has wrong group type", var.c_str());
-        const int group_dim1 = CCTK_GroupDimI(groupindex);
-        if (group_dim1 != variable_dimensions)
-          CCTK_VERROR("Variable \"%s\" has wrong dimension", var.c_str());
-      }
+      if (group_type1 != group_type)
+        CCTK_VERROR("Variable \"%s\" has wrong group type", var.c_str());
+      const int group_dim1 = CCTK_GroupDimI(groupindex);
+      if (group_dim1 != variable_dimensions)
+        CCTK_VERROR("Variable \"%s\" has wrong dimension", var.c_str());
     }
-
-    // TODO: Check whether directions are unique
-    output_directions = ofd.output_directions;
-    for (const auto &dir : output_directions)
-      if (dir < 0 || dir >= variable_dimensions)
-        CCTK_VERROR("Bad output direction %d", dir);
-
-    reductions = ofd.reductions;
-
-    if (!output_directions.empty() && !reductions.empty())
-      CCTK_ERROR("Cannot have both output directions and reductions");
-
-    // TODO: Maintain a list of known output formats?
-    format_name = ofd.format_name;
-    if (format_name == "")
-      CCTK_VERROR("Bad format name \"%s\"", format_name.c_str());
-
-    // TODO: Maintain a list of known versions for each output format?
-    format_version = ofd.format_version;
-    for (const auto &ver : format_version)
-      if (ver < 0)
-        CCTK_VERROR("Bad format version %d.%d.%d", format_version[0],
-                    format_version[1], format_version[2]);
   }
 
-  YAML::Emitter &operator<<(YAML::Emitter &yaml,
-                            const real_output_file_description_t &rofd) {
-    yaml << YAML::LocalTag("cactus-metadata-1.0.0");
-    yaml << YAML::BeginMap;
-    yaml << YAML::Key << "filename" << YAML::Value << rofd.filename;
-    yaml << YAML::Key << "description" << YAML::Value << rofd.description;
-    yaml << YAML::Key << "writer_thorn" << YAML::Value << rofd.writer_thorn;
-    yaml << YAML::Key << "variables" << YAML::Value << rofd.variables;
-    if (!rofd.variables.empty()) {
-      yaml << YAML::Key << "iterations" << YAML::Value << YAML::Flow
-           << rofd.iterations;
-      yaml << YAML::Key << "group_type" << YAML::Value << rofd.group_type;
-      yaml << YAML::Key << "variable_dimensions" << YAML::Value
-           << rofd.variable_dimensions;
-      yaml << YAML::Key << "real_output_directions" << YAML::Value << YAML::Flow
-           << rofd.output_directions;
-      yaml << YAML::Key << "reductions" << YAML::Value << YAML::Flow
-           << rofd.reductions;
-    } // if !rofd.variables.empty
-    yaml << YAML::Key << "format_name" << YAML::Value << rofd.format_name;
-    yaml << YAML::Key << "format_version" << YAML::Value << YAML::Flow
-         << YAML::BeginSeq;
-    for (const auto &elt : rofd.format_version)
-      yaml << elt;
-    yaml << YAML::EndSeq;
-    yaml << YAML::EndMap;
-    return yaml;
-  }
+  // TODO: Check whether directions are unique
+  output_directions = ofd.output_directions;
+  for (const auto &dir : output_directions)
+    if (dir < 0 || dir >= variable_dimensions)
+      CCTK_VERROR("Bad output direction %d", dir);
 
-  ////////////////////////////////////////////////////////////////////////////////
+  reductions = ofd.reductions;
 
-  std::vector<real_output_file_description_t> real_output_file_descriptions;
+  if (!output_directions.empty() && !reductions.empty())
+    CCTK_ERROR("Cannot have both output directions and reductions");
 
-  void OutputMeta_RegisterOutputFile(
-      output_file_description_t output_file_description) {
-    // Only the root process outputs metadata
-    if (CCTK_MyProc(nullptr) != 0)
-      return;
+  // TODO: Maintain a list of known output formats?
+  format_name = ofd.format_name;
+  if (format_name == "")
+    CCTK_VERROR("Bad format name \"%s\"", format_name.c_str());
 
-    real_output_file_descriptions.emplace_back(
-        std::move(output_file_description));
-  }
+  // TODO: Maintain a list of known versions for each output format?
+  format_version = ofd.format_version;
+  for (const auto &ver : format_version)
+    if (ver < 0)
+      CCTK_VERROR("Bad format version %d.%d.%d", format_version[0],
+                  format_version[1], format_version[2]);
+}
 
-  void OutputMeta(const cGH *restrict const cctkGH) {
-    DECLARE_CCTK_PARAMETERS;
+YAML::Emitter &operator<<(YAML::Emitter &yaml,
+                          const real_output_file_description_t &rofd) {
+  yaml << YAML::LocalTag("cactus-metadata-1.0.0");
+  yaml << YAML::BeginMap;
+  yaml << YAML::Key << "filename" << YAML::Value << rofd.filename;
+  yaml << YAML::Key << "description" << YAML::Value << rofd.description;
+  yaml << YAML::Key << "writer_thorn" << YAML::Value << rofd.writer_thorn;
+  yaml << YAML::Key << "variables" << YAML::Value << rofd.variables;
+  if (!rofd.variables.empty()) {
+    yaml << YAML::Key << "iterations" << YAML::Value << YAML::Flow
+         << rofd.iterations;
+    yaml << YAML::Key << "group_type" << YAML::Value << rofd.group_type;
+    yaml << YAML::Key << "variable_dimensions" << YAML::Value
+         << rofd.variable_dimensions;
+    yaml << YAML::Key << "real_output_directions" << YAML::Value << YAML::Flow
+         << rofd.output_directions;
+    yaml << YAML::Key << "reductions" << YAML::Value << YAML::Flow
+         << rofd.reductions;
+  } // if !rofd.variables.empty
+  yaml << YAML::Key << "format_name" << YAML::Value << rofd.format_name;
+  yaml << YAML::Key << "format_version" << YAML::Value << YAML::Flow
+       << YAML::BeginSeq;
+  for (const auto &elt : rofd.format_version)
+    yaml << elt;
+  yaml << YAML::EndSeq;
+  yaml << YAML::EndMap;
+  return yaml;
+}
 
-    // Only the root process outputs metadata
-    const bool is_root = CCTK_MyProc(nullptr) == 0;
-    if (!is_root)
-      return;
+////////////////////////////////////////////////////////////////////////////////
 
-    // Skip metadata output if no files were written
-    if (real_output_file_descriptions.empty())
-      return;
+std::vector<real_output_file_description_t> real_output_file_descriptions;
 
-    YAML::Emitter yaml;
-    yaml << YAML::Comment("Cactus Metadata");
-    yaml << YAML::BeginDoc;
-    yaml << real_output_file_descriptions;
-    yaml << YAML::EndDoc;
+void OutputMeta_RegisterOutputFile(
+    output_file_description_t output_file_description) {
+  // Only the root process outputs metadata
+  if (CCTK_MyProc(nullptr) != 0)
+    return;
 
-    std::ostringstream buf;
-    buf << out_dir << "/cactus-metadata"
-        << ".it" << std::setw(8) << std::setfill('0') << cctkGH->cctk_iteration
-        << ".yaml";
-    const std::string filename = buf.str();
-    std::ofstream file(filename);
-    file << yaml.c_str() << "\n";
-    file.close();
+  real_output_file_descriptions.emplace_back(
+      std::move(output_file_description));
+}
 
-    real_output_file_descriptions.clear();
-  }
+void OutputMeta(const cGH *restrict const cctkGH) {
+  DECLARE_CCTK_PARAMETERS;
+
+  // Only the root process outputs metadata
+  const bool is_root = CCTK_MyProc(nullptr) == 0;
+  if (!is_root)
+    return;
+
+  // Skip metadata output if no files were written
+  if (real_output_file_descriptions.empty())
+    return;
+
+  YAML::Emitter yaml;
+  yaml << YAML::Comment("Cactus Metadata");
+  yaml << YAML::BeginDoc;
+  yaml << real_output_file_descriptions;
+  yaml << YAML::EndDoc;
+
+  std::ostringstream buf;
+  buf << out_dir << "/cactus-metadata"
+      << ".it" << std::setw(8) << std::setfill('0') << cctkGH->cctk_iteration
+      << ".yaml";
+  const std::string filename = buf.str();
+  std::ofstream file(filename);
+  file << yaml.c_str() << "\n";
+  file.close();
+
+  real_output_file_descriptions.clear();
+}
 
 } // namespace CarpetX
