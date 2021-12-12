752dfc251701d35845ae4a88c28ba76c31e27d99
Erik Schnetter <schnetter@gmail.com>
2021-12-12 00:25:34 -0500

    HydroBase: Run on GPUs

--- HydroBase/src/hydrobase.cxx
+++ HydroBase/src/hydrobase.cxx
@@ -1,8 +1,11 @@
-#include <loop.hxx>
+#include <loop_device.hxx>
 
+#include <fixmath.hxx>
 #include <cctk.h>
 #include <cctk_Arguments_Checked.h>
 #include <cctk_Parameters.h>
+
+#include <array>
 
 namespace HydroBase {
 using namespace Loop;
@@ -11,22 +14,30 @@ extern "C" void HydroBase_initial_data(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS_HydroBase_initial_data;
   DECLARE_CCTK_PARAMETERS;
 
-  const GF3D<CCTK_REAL, 1, 1, 1> rho_(cctkGH, rho);
+  const std::array<int, dim> indextype = {1, 1, 1};
+  const GF3D2layout layout(cctkGH, indextype);
 
-  const GF3D<CCTK_REAL, 1, 1, 1> velx_(cctkGH, velx);
-  const GF3D<CCTK_REAL, 1, 1, 1> vely_(cctkGH, vely);
-  const GF3D<CCTK_REAL, 1, 1, 1> velz_(cctkGH, velz);
+  const GF3D2<CCTK_REAL> rho_(layout, rho);
 
-  const GF3D<CCTK_REAL, 1, 1, 1> eps_(cctkGH, eps);
+  const GF3D2<CCTK_REAL> velx_(layout, velx);
+  const GF3D2<CCTK_REAL> vely_(layout, vely);
+  const GF3D2<CCTK_REAL> velz_(layout, velz);
 
-  const GF3D<CCTK_REAL, 1, 1, 1> press_(cctkGH, press);
+  const GF3D2<CCTK_REAL> eps_(layout, eps);
 
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { rho_(p.I) = 0; });
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { velx_(p.I) = 0; });
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { vely_(p.I) = 0; });
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { velz_(p.I) = 0; });
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { eps_(p.I) = 0; });
-  loop_all<1, 1, 1>(cctkGH, [&](const PointDesc &p) { press_(p.I) = 0; });
+  const GF3D2<CCTK_REAL> press_(layout, press);
+
+  const GridDescBaseDevice grid(cctkGH);
+  grid.loop_all_device<1, 1, 1>(grid.nghostzones,
+                                [=] CCTK_DEVICE(const PointDesc &p)
+                                    CCTK_ATTRIBUTE_ALWAYS_INLINE {
+                                      rho_(p.I) = 0;
+                                      velx_(p.I) = 0;
+                                      vely_(p.I) = 0;
+                                      velz_(p.I) = 0;
+                                      eps_(p.I) = 0;
+                                      press_(p.I) = 0;
+                                    });
 }
 
 } // namespace HydroBase
