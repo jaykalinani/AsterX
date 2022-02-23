b03374c927a21171b811df70d583c25ab99c5def
Erik Schnetter <schnetter@gmail.com>
2022-02-23 13:41:05 -0500

    GRHydroToyGPU: Use multi-argument max

--- GRHydroToyGPU/src/estimate_error.cxx
+++ GRHydroToyGPU/src/estimate_error.cxx
@@ -4,25 +4,12 @@
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
 
+#include <algorithm>
 #include <cmath>
 
 namespace GRHydroToyGPU {
 using namespace std;
 using namespace Loop;
-
-////////////////////////////////////////////////////////////////////////////////
-
-template <typename T>
-inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_DEVICE CCTK_HOST T fmax6(T x0, T x1,
-                                                                  T x2, T x3,
-                                                                  T x4, T x5) {
-  T r01 = fmax(x0, x1);
-  T r23 = fmax(x2, x3);
-  T r014 = fmax(r01, x4);
-  T r01234 = fmax(r014, r23);
-  T r012345 = fmax(r01234, x5);
-  return r012345;
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -41,13 +28,13 @@ extern "C" void GRHydroToyGPU_EstimateError(CCTK_ARGUMENTS) {
             auto var0 = gf_var(p.I);
             auto varp = gf_var(p.I + DI[d]);
             // Calculate derivative
-            err = fmax(err, fmax(fabs(var0 - varm), fabs(varp - var0)));
+            err = max({err, fabs(var0 - varm), fabs(varp - var0)});
           }
           return err;
         };
 
-        regrid_error(p.I) = fmax6(calcerr(rho), calcerr(velx), calcerr(vely),
-                                  calcerr(velz), calcerr(eps), calcerr(press));
+        regrid_error(p.I) = max({calcerr(rho), calcerr(velx), calcerr(vely),
+                                 calcerr(velz), calcerr(eps), calcerr(press)});
       });
 }
 
