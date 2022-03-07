74ff3911fd2dbbfb044e0a7164b64b8f1702a059
Erik Schnetter <schnetter@gmail.com>
2022-03-07 10:39:06 -0500

    HydroBase: Add Bvec variables

--- HydroBase/src/hydrobase.cxx
+++ HydroBase/src/hydrobase.cxx
@@ -5,38 +5,25 @@
 #include <cctk_Arguments.h>
 #include <cctk_Parameters.h>
 
-#include <array>
-
 namespace HydroBase {
 using namespace Loop;
 
 extern "C" void HydroBase_initial_data(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_ARGUMENTS_HydroBase_initial_data;
+  DECLARE_CCTK_ARGUMENTSX_HydroBase_initial_data;
   DECLARE_CCTK_PARAMETERS;
 
-  const std::array<int, dim> indextype = {1, 1, 1};
-  const GF3D2layout layout(cctkGH, indextype);
-
-  const GF3D2<CCTK_REAL> rho_(layout, rho);
-
-  const GF3D2<CCTK_REAL> velx_(layout, velx);
-  const GF3D2<CCTK_REAL> vely_(layout, vely);
-  const GF3D2<CCTK_REAL> velz_(layout, velz);
-
-  const GF3D2<CCTK_REAL> eps_(layout, eps);
-
-  const GF3D2<CCTK_REAL> press_(layout, press);
-
-  const GridDescBaseDevice grid(cctkGH);
   grid.loop_all_device<1, 1, 1>(grid.nghostzones,
                                 [=] CCTK_DEVICE(const PointDesc &p)
                                     CCTK_ATTRIBUTE_ALWAYS_INLINE {
-                                      rho_(p.I) = 0;
-                                      velx_(p.I) = 0;
-                                      vely_(p.I) = 0;
-                                      velz_(p.I) = 0;
-                                      eps_(p.I) = 0;
-                                      press_(p.I) = 0;
+                                      rho(p.I) = 0;
+                                      velx(p.I) = 0;
+                                      vely(p.I) = 0;
+                                      velz(p.I) = 0;
+                                      eps(p.I) = 0;
+                                      press(p.I) = 0;
+                                      Bvecx(p.I) = 0;
+                                      Bvecy(p.I) = 0;
+                                      Bvecz(p.I) = 0;
                                     });
 }
 
