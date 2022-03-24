28240517604112b935dff080be9ae197967393d4
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2022-03-24 11:09:51 -0300

    Completed local identity tests implementation. All tests are passing

--- MultiPatch/src/cake/cake_tests.cxx
+++ MultiPatch/src/cake/cake_tests.cxx
@@ -45,7 +45,6 @@ std::string patch_owner_test(const PatchTransformations &pt,
 std::string global_identity_test(const PatchTransformations &pt,
                                  const MultiPatch::Cake::svec_u &global_vars) {
 
-  using MultiPatch::Cake::svec_u;
   using MultiPatchTests::colored;
   using MultiPatchTests::isapprox;
   using MultiPatchTests::string_color;
@@ -55,8 +54,7 @@ std::string global_identity_test(const PatchTransformations &pt,
   const auto g2l = pt.global2local(pt, global_vars);
 
   const auto owner_patch_idx = std::get<0>(g2l);
-  const svec_u local_vars = {(std::get<1>(g2l))(0), (std::get<1>(g2l))(1),
-                             (std::get<1>(g2l))(2)};
+  const auto local_vars = std::get<1>(g2l);
 
   const auto l2g = pt.local2global(pt, owner_patch_idx, local_vars);
 
@@ -95,6 +93,66 @@ std::string global_identity_test(const PatchTransformations &pt,
   return msg;
 }
 
+/**
+ * Tests if global2local(local2global(local, patch)) == (local, patch)
+ * TODO: doc
+ */
+std::string local_identity_test(const PatchTransformations &pt, int patch,
+                                const MultiPatch::Cake::svec_u &local_point) {
+
+  using MultiPatch::Cake::patch_piece;
+  using MultiPatch::Cake::piece_name;
+  using MultiPatchTests::colored;
+  using MultiPatchTests::isapprox;
+  using MultiPatchTests::string_color;
+
+  std::string msg{"has "};
+
+  const auto l2g = pt.local2global(pt, patch, local_point);
+  const auto g2l = pt.global2local(pt, l2g);
+
+  const auto computed_patch_idx = std::get<0>(g2l);
+  const auto computed_local_point = std::get<1>(g2l);
+
+  const bool test1 = computed_patch_idx == patch;
+  const bool test2 = isapprox(computed_local_point(0), local_point(0));
+  const bool test3 = isapprox(computed_local_point(1), local_point(1));
+  const bool test4 = isapprox(computed_local_point(2), local_point(2));
+
+  if (test1 && test2 && test3 && test4) {
+    msg += colored<string_color::green>("PASSED");
+  } else {
+    msg += colored<string_color::red>("FAILED");
+    msg += ". Reason: ";
+
+    if (!test1) {
+      msg += "the computed patch is ";
+      msg += piece_name(static_cast<patch_piece>(computed_patch_idx));
+      msg += ". ";
+    }
+
+    if (!test2) {
+      msg += "the computed local coordinate a is ";
+      msg += std::to_string(computed_local_point(0));
+      msg += ". ";
+    }
+
+    if (!test3) {
+      msg += "the computed local coordinate b is ";
+      msg += std::to_string(computed_local_point(1));
+      msg += ". ";
+    }
+
+    if (!test4) {
+      msg += "the computed local coordinate c is ";
+      msg += std::to_string(computed_local_point(2));
+      msg += ". ";
+    }
+  }
+
+  return msg;
+}
+
 } // namespace CakeTests
 } // namespace MultiPatch
 
@@ -102,12 +160,14 @@ extern "C" void run_cake_tests() {
   DECLARE_CCTK_PARAMETERS
 
   using std::mt19937_64;
+  using std::uniform_int_distribution;
   using std::uniform_real_distribution;
 
   using std::cos;
   using std::sin;
 
   using MultiPatch::Cake::patch_piece;
+  using MultiPatch::Cake::piece_name;
   using MultiPatch::Cake::svec_u;
 
   using MultiPatch::dim;
@@ -200,24 +260,39 @@ extern "C" void run_cake_tests() {
 
   uniform_real_distribution<CCTK_REAL> local_distrib(-1.0, 1.0);
 
+  uniform_int_distribution<int> patch_distrib(0, 6);
+
   auto global_point = svec_u{0.0, 0.0, 0.0};
+  auto local_point = svec_u{0.0, 0.0, 0.0};
+  double r = 0.0, theta = 0.0, phi = 0.0;
+  int patch = 0;
 
   // Tests if local2global(global2local(global)) == global
   for (int i = 0; i < repeat_tests; i++) {
-    const double r = r_distrib(engine);
-    const double theta = theta_distrib(engine);
-    const double phi = phi_distrib(engine);
+    r = r_distrib(engine);
+    theta = theta_distrib(engine);
+    phi = phi_distrib(engine);
 
     global_point = {r * sin(theta) * cos(phi), r * sin(theta) * sin(phi),
                     r * cos(theta)};
 
-    const auto owner_patch =
-        MultiPatch::Cake::get_owner_patch(pt, global_point);
-
     CCTK_VINFO("  local2global(global2local(global)) transformation test at "
-               "point (%f, %f, %f) patch %s %s",
+               "point (%f, %f, %f) %s",
                global_point(0), global_point(1), global_point(2),
-               MultiPatch::Cake::piece_name(owner_patch).c_str(),
                global_identity_test(pt, global_point).c_str());
+  }
+
+  // Tests if global2local(local2global(local, patch)) == (local, patch)
+  for (int i = 0; i < repeat_tests; i++) {
+    local_point = {local_distrib(engine), local_distrib(engine),
+                   local_distrib(engine)};
+
+    patch = patch_distrib(engine);
+
+    CCTK_VINFO("  global2local(local2global(local, patch)) transformation test "
+               "at point (%f, %f, %f) patch %s %s",
+               local_point(0), local_point(1), local_point(2),
+               piece_name(static_cast<patch_piece>(patch)).c_str(),
+               local_identity_test(pt, patch, local_point).c_str());
   }
 }
