58ed194f6c1619a416a2fcb3a8a2876aaf2c6d3e
Erik Schnetter <schnetter@gmail.com>
2022-04-16 17:51:50 -0400

    Z4c: Don't apply boundary conditions

--- Z4c/src/sync2.cxx
+++
@@ -1,9 +0,0 @@
-#include <fixmath.hxx> // include this before <cctk.h>
-#include <cctk.h>
-#include <cctk_Arguments.h>
-
-namespace Z4c {
-
-extern "C" void Z4c_Sync2(CCTK_ARGUMENTS) { DECLARE_CCTK_ARGUMENTS_Z4c_Sync2; }
-
-} // namespace Z4c
