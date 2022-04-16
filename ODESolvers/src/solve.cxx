6b9f84b17aae6773680cd65619d735d9af27592b
Erik Schnetter <schnetter@gmail.com>
2022-04-16 17:31:56 -0400

    ODESolvers: Update code for recent CarpetX changes

--- ODESolvers/src/solve.cxx
+++ ODESolvers/src/solve.cxx
@@ -66,7 +66,7 @@ struct statecomp_t {
   statecomp_t(const statecomp_t &) = delete;
   statecomp_t &operator=(const statecomp_t &) = delete;
 
-  const cGH *cctkGH;            // this might be unused
+  const cGH *cctkGH; // this might be unused
   vector<string> groupnames;
   vector<int> groupids;
   vector<amrex::MultiFab *> mfabs;
@@ -113,7 +113,8 @@ void statecomp_t::check_valid(const function<string()> &why) const {
         const auto &groupdata = *leveldata.groupdata.at(groupid);
         for (int vi = 0; vi < groupdata.numvars; ++vi) {
           const int tl = 0;
-          CarpetX::check_valid(groupdata, vi, tl, why);
+          CarpetX::check_valid(groupdata, vi, tl, nan_handling_t::forbid_nans,
+                               why);
         }
       });
     }
