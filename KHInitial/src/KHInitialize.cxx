176a868422d20298bb84f29e15c9c5275b6831c2
Erik Schnetter <schnetter@gmail.com>
2022-05-27 10:54:50 -0400

    KHInitial: Modernize and clean code

--- KHInitial/src/KHInitialize.cxx
+++ KHInitial/src/KHInitialize.cxx
@@ -4,64 +4,48 @@
 #include <cctk_Parameters.h>
 
 #include <cmath>
-#include <random>
 
-//initial setup adapted from Springel+2010
+// initial setup adapted from Springel+2010
 
 namespace KHInitial {
-using namespace std;
-using namespace Loop;
-
-template <typename T>
-inline CCTK_ATTRIBUTE_ALWAYS_INLINE CCTK_DEVICE CCTK_HOST T pow2(T x) {
-  return x * x;
-}
 
 extern "C" void KHInitial_Initialize(CCTK_ARGUMENTS) {
-  DECLARE_CCTK_ARGUMENTS_KHInitial_Initialize;
+  DECLARE_CCTK_ARGUMENTSX_KHInitial_Initialize;
   DECLARE_CCTK_PARAMETERS;
-
-  const GridDescBaseDevice grid(cctkGH);
-  constexpr array<int, dim> cell_centred = {1, 1, 1};
-  const GF3D2layout gf_layout_cell(cctkGH, cell_centred);
-
-  const GF3D2<CCTK_REAL> gf_rho(gf_layout_cell, rho);
-  const GF3D2<CCTK_REAL> gf_velx(gf_layout_cell, velx);
-  const GF3D2<CCTK_REAL> gf_vely(gf_layout_cell, vely);
-  const GF3D2<CCTK_REAL> gf_velz(gf_layout_cell, velz);
-  const GF3D2<CCTK_REAL> gf_press(gf_layout_cell, press);
-  const GF3D2<CCTK_REAL> gf_eps(gf_layout_cell, eps);
 
   if (CCTK_EQUALS(initial_hydro, "KHI")) {
 
     grid.loop_all_device<1, 1, 1>(
         grid.nghostzones,
-        [=] CCTK_DEVICE(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
-
-	  //set velocities in opposite directions in x along the slip surface 
-	  //set slightly different densities along the slip surface
-	  //location of slip surfaces hardcoded at (abs(p.y) = 0.25)
+        [=] CCTK_DEVICE(const Loop::PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
+          // set velocities in opposite directions in x along the slip surface
+          // set slightly different densities along the slip surface
+          // location of slip surfaces hardcoded at (abs(p.y) = 0.25)
+          using std::abs;
           if (abs(p.y) >= 0.25) {
-            gf_rho(p.I) = rhoUp;
-            gf_velx(p.I) = vxUp;
+            rho(p.I) = rhoUp;
+            velx(p.I) = vxUp;
           } else {
-            gf_rho(p.I) = rhoLow;
-            gf_velx(p.I) = vxLow;
+            rho(p.I) = rhoLow;
+            velx(p.I) = vxLow;
           }
-          //excite the instability by peturbing v^y
-	  gf_vely(p.I) = w0*sin(4*M_PI*p.x) * ( exp(-pow2(p.y-0.25)/(2 * pow2(sigma))) + exp(-pow2(p.y+0.25)/(2*pow2(sigma))) );
-	  gf_velz(p.I) = 0.0;
-          
-	  //set constant initial pressure throughout the domain
-	  gf_press(p.I) = p_val;
+          // excite the instability by peturbing v^y
+          using std::exp, std::pow, std::sin;
+          vely(p.I) = w0 * sin(4 * M_PI * p.x) *
+                      (exp(-pow(p.y - 0.25, 2) / (2 * pow(sigma, 2))) +
+                       exp(-pow(p.y + 0.25, 2) / (2 * pow(sigma, 2))));
+          velz(p.I) = 0.0;
 
-	  // TODO: compute eps using EOS driver
+          // set constant initial pressure throughout the domain
+          press(p.I) = p_val;
+
+          // TODO: compute eps using EOS driver
           // for now, using ideal gas EOS
-          gf_eps(p.I) = gf_press(p.I) / (gf_rho(p.I) * (gamma - 1));
+          eps(p.I) = press(p.I) / (rho(p.I) * (gamma - 1));
         });
 
   } else {
-    CCTK_ERROR("Incorrect test case!");
+    CCTK_ERROR("Internal error");
   }
 }
 
