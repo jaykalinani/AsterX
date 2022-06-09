5c99a6d46e9fe814800f87bca02cd57e2ec3870a
Erik Schnetter <schnetter@gmail.com>
2022-06-08 22:34:35 -0400

    AsterX: Don't use #define for constants

--- AsterX/src/con2prim.h
+++ AsterX/src/con2prim.h
@@ -1,23 +1,19 @@
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#ifndef CON2PRIM_H
+#define CON2PRIM_H
 
-#define NCONS 5
-#define NPRIMS 5
+constexpr int NCONS = 5;
+constexpr int NPRIMS = 5;
 
-#define D 0
-#define S1_cov 1
-#define S2_cov 2
-#define S3_cov 3
-#define TAU 4
+constexpr int D = 0;
+constexpr int S1_cov = 1;
+constexpr int S2_cov = 2;
+constexpr int S3_cov = 3;
+constexpr int TAU = 4;
 
-#define RHO 0
-#define V1_COV 1
-#define V2_COV 2
-#define V3_COV 3
-#define EPS 4
+constexpr int RHO = 0;
+constexpr int V1_COV = 1;
+constexpr int V2_COV = 2;
+constexpr int V3_COV = 3;
+constexpr int EPS = 4;
 
-#ifdef __cplusplus
-}
-#endif
+#endif // #ifndef CON2PRIM_H
