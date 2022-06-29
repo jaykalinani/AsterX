d85ba1435f1b52b9f814b2352a77517a80883e59
Erik Schnetter <schnetter@gmail.com>
2022-06-29 15:09:43 -0400

    CarpetX: Move norm output into its own source file

---
+++ CarpetX/src/io_norm.hxx
@@ -0,0 +1,13 @@
+#ifndef IO_NORM_HXX
+#define IO_NORM_HXX
+
+#include <fixmath.hxx>
+#include <cctk.h>
+
+namespace CarpetX {
+
+void OutputNorms(const cGH *restrict cctkGH);
+
+}
+
+#endif // #define IO_NORM_HXX
