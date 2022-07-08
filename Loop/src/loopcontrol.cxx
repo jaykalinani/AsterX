75f10d6c5509cfaf15b804a231f72ce5249e8081
Erik Schnetter <schnetter@gmail.com>
2022-07-08 09:38:28 -0400

    Loop: New thorn

---
+++ Loop/src/loopcontrol.cxx
@@ -0,0 +1,28 @@
+#include <fixmath.hxx>
+#include <cctk.h>
+
+#include "loop.hxx"
+#include "loopcontrol.h"
+
+extern "C" GridDescBase_t LC_CreateGridDesc(const cGH *cctkGH) {
+  static_assert(LC_DIM == Loop::dim, "");
+  const Loop::GridDescBase lgrid(cctkGH);
+  GridDescBase_t grid;
+
+  for (int d = 0; d < LC_DIM; ++d) {
+    grid.gsh[d] = lgrid.gsh[d];
+    grid.lbnd[d] = lgrid.lbnd[d];
+    grid.ubnd[d] = lgrid.ubnd[d];
+    grid.lsh[d] = lgrid.lsh[d];
+    grid.ash[d] = lgrid.ash[d];
+    grid.bbox[2 * d + 0] = lgrid.bbox[2 * d + 0];
+    grid.bbox[2 * d + 1] = lgrid.bbox[2 * d + 1];
+    grid.nghostzones[d] = lgrid.nghostzones[d];
+    grid.tmin[d] = lgrid.tmin[d];
+    grid.tmax[d] = lgrid.tmax[d];
+    grid.x0[d] = lgrid.x0[d];
+    grid.dx[d] = lgrid.dx[d];
+  }
+
+  return grid;
+}
