dec225c2993587bf9e04a845f1b8675bcefb5718
Roland Haas <rhaas@illinois.edu>
2022-08-05 06:43:12 -0500

    TOVSolver: initial import fro einsteininitialdata
    this is git hash 4785cbfb9857a13f1b72a5285a7d05922fcbbc20

---
+++ TOVSolver/src/tov.h
@@ -0,0 +1,2 @@
+
+void TOVSolverC_export_local_variables(CCTK_REAL **exported_TOV_press_1d, CCTK_REAL **exported_TOV_m_1d, CCTK_REAL **exported_TOV_phi_1d, CCTK_REAL **exported_TOV_rbar_1d, CCTK_REAL **exported_TOV_r_1d );
