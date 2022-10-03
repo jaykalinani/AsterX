6cf6fcbae66b081b7af0f1c3df60867da8413941
Liwei Ji <jiliwei.phys@gmail.com>
2022-10-03 20:39:34 +0000

    AsterX: Add new option '1st grad of rho' for regrid method

--- AsterX/src/estimate_error.cxx
+++ AsterX/src/estimate_error.cxx
@@ -11,12 +11,34 @@ namespace AsterX {
 using namespace std;
 using namespace Loop;
 
-enum class regrid_t { first_deriv_all, first_deriv_rho, second_deriv_rho };
+enum class regrid_t {
+  first_deriv_all,
+  first_deriv_rho,
+  second_deriv_rho,
+  first_grad_rho
+};
 
 /* calculate max of d(var)/(dx) * hx in all dirs */
 template <typename T>
-CCTK_DEVICE CCTK_HOST const T calc_err_1st(const GF3D2<const T> &gf,
-                                           const PointDesc &p) {
+CCTK_DEVICE CCTK_HOST const T calc_grad_1st(const GF3D2<const T> &gf,
+                                            const PointDesc &p) {
+  constexpr auto DI = PointDesc::DI;
+  CCTK_REAL err{0}, errp{0}, errm{0};
+  for (int d = 0; d < dim; ++d) {
+    auto varm = gf(p.I - DI[d]);
+    auto var0 = gf(p.I);
+    auto varp = gf(p.I + DI[d]);
+    errp += (varp - var0) * (varp - var0);
+    errm += (var0 - varm) * (var0 - varm);
+  }
+  err = max({errp, errm});
+  return sqrt(err);
+}
+
+/* calculate max of d(var)/(dx) * hx in all dirs */
+template <typename T>
+CCTK_DEVICE CCTK_HOST const T calc_deriv_1st(const GF3D2<const T> &gf,
+                                             const PointDesc &p) {
   constexpr auto DI = PointDesc::DI;
   CCTK_REAL err{0};
   for (int d = 0; d < dim; ++d) {
@@ -30,8 +52,8 @@ CCTK_DEVICE CCTK_HOST const T calc_err_1st(const GF3D2<const T> &gf,
 
 /* calculate max of d^2(var)/(dx^2) * hx^2 in all dirs */
 template <typename T>
-CCTK_DEVICE CCTK_HOST const T calc_err_2nd(const GF3D2<const T> &gf,
-                                           const PointDesc &p) {
+CCTK_DEVICE CCTK_HOST const T calc_deriv_2nd(const GF3D2<const T> &gf,
+                                             const PointDesc &p) {
   constexpr auto DI = PointDesc::DI;
   CCTK_REAL err{0};
   for (int d = 0; d < dim; ++d) {
@@ -56,6 +78,8 @@ extern "C" void AsterX_EstimateError(CCTK_ARGUMENTS) {
     regrid = regrid_t::first_deriv_rho;
   else if (CCTK_EQUALS(regrid_method, "2nd deriv of rho"))
     regrid = regrid_t::second_deriv_rho;
+  else if (CCTK_EQUALS(regrid_method, "1st grad of rho"))
+    regrid = regrid_t::first_grad_rho;
   else
     CCTK_ERROR("Unknown value for parameter \"regrid_method\"");
 
@@ -66,19 +90,24 @@ extern "C" void AsterX_EstimateError(CCTK_ARGUMENTS) {
 
         case regrid_t::first_deriv_all: {
           regrid_error(p.I) =
-              max({calc_err_1st(rho, p), calc_err_1st(velx, p),
-                   calc_err_1st(vely, p), calc_err_1st(velz, p),
-                   calc_err_1st(eps, p), calc_err_1st(press, p)});
+              max({calc_deriv_1st(rho, p), calc_deriv_1st(velx, p),
+                   calc_deriv_1st(vely, p), calc_deriv_1st(velz, p),
+                   calc_deriv_1st(eps, p), calc_deriv_1st(press, p)});
           break;
         }
 
         case regrid_t::first_deriv_rho: {
-          regrid_error(p.I) = calc_err_1st(rho, p);
+          regrid_error(p.I) = calc_deriv_1st(rho, p);
           break;
         }
 
         case regrid_t::second_deriv_rho: {
-          regrid_error(p.I) = calc_err_2nd(rho, p);
+          regrid_error(p.I) = calc_deriv_2nd(rho, p);
+          break;
+        }
+
+        case regrid_t::first_grad_rho: {
+          regrid_error(p.I) = calc_grad_1st(rho, p);
           break;
         }
 
