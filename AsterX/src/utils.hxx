07eaa6512c144948d76c05d35745ea6dd9410fac
Jay Kalinani <jaykalinani@gmail.com>
2022-10-19 12:14:27 -0700

    AsterX: Add helper function for interpolation from center to edges

--- AsterX/src/utils.hxx
+++ AsterX/src/utils.hxx
@@ -257,4 +257,21 @@ CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T calc_avg_v2f(const G
   return gf_avg / 4.0;
 }
 
+// Second-order average of cell-centered grid functions to edge center
+template <typename T>
+CCTK_DEVICE CCTK_HOST CCTK_ATTRIBUTE_ALWAYS_INLINE inline T calc_avg_c2e(const GF3D2<const T> &gf,
+                                     const PointDesc &p, const int dir) {
+  constexpr auto DI = PointDesc::DI;
+  T gf_avg = 0.0;
+
+  for (int dk = 0; dk < (dir == 2 ? 1 : 2); ++dk) {
+    for (int dj = 0; dj < (dir == 1 ? 1 : 2); ++dj) {
+      for (int di = 0; di < (dir == 0 ? 1 : 2); ++di) {
+        gf_avg += gf(p.I - DI[0] * di - DI[1] * dj - DI[2] * dk);
+      }
+    }
+  }
+  return gf_avg / 4.0;
+}
+
 } // namespace AsterX
