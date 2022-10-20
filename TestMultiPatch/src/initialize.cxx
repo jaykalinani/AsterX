faa0c002d241f4936c702ca7fddc7daee3785bc5
Erik Schnetter <schnetter@gmail.com>
2022-10-20 13:58:36 -0400

    TestMultiPatch: Don't use up/down tensor index kinds

--- TestMultiPatch/src/initialize.cxx
+++ TestMultiPatch/src/initialize.cxx
@@ -15,19 +15,18 @@
 namespace TestMultiPatch {
 
 /**
- * Stores a 3-vector with all indices up
+ * Stores a 3-vector
  */
-template <typename real_t>
-using svec_u = Arith::vec<real_t, Loop::dim, Arith::UP>;
+template <typename real_t> using svec = Arith::vec<real_t, Loop::dim>;
 
 /**
- * Stores a 4-vector with all indices up
+ * Stores a 4-vector
  */
-template <typename real_t> using qvec_u = Arith::vec<real_t, 4, Arith::UP>;
+template <typename real_t> using qvec = Arith::vec<real_t, 4>;
 
 template <typename real_t>
 CCTK_DEVICE CCTK_HOST static inline real_t
-plane_wave_omega(const svec_u<real_t> &wave_number) {
+plane_wave_omega(const svec<real_t> &wave_number) {
   using std::sqrt;
   return sqrt(wave_number(0) * wave_number(0) +
               wave_number(1) * wave_number(1) +
@@ -35,20 +34,19 @@ plane_wave_omega(const svec_u<real_t> &wave_number) {
 }
 
 template <typename real_t>
-CCTK_DEVICE CCTK_HOST static inline qvec_u<real_t>
-plane_wave_n(const svec_u<real_t> &wave_number, const qvec_u<real_t> &offsets,
-             const qvec_u<real_t> coords) {
-  return qvec_u<real_t>{plane_wave_omega(wave_number) *
-                            (coords(0) - offsets(0)),
-                        wave_number(0) * (coords(1) - offsets(1)),
-                        wave_number(1) * (coords(2) - offsets(2)),
-                        wave_number(2) * (coords(3) - offsets(3))};
+CCTK_DEVICE CCTK_HOST static inline qvec<real_t>
+plane_wave_n(const svec<real_t> &wave_number, const qvec<real_t> &offsets,
+             const qvec<real_t> coords) {
+  return qvec<real_t>{plane_wave_omega(wave_number) * (coords(0) - offsets(0)),
+                      wave_number(0) * (coords(1) - offsets(1)),
+                      wave_number(1) * (coords(2) - offsets(2)),
+                      wave_number(2) * (coords(3) - offsets(3))};
 }
 
 template <typename real_t>
 CCTK_DEVICE CCTK_HOST static inline real_t
-plane_wave_w(const svec_u<real_t> &wave_number, const qvec_u<real_t> &offsets,
-             const qvec_u<real_t> coords) {
+plane_wave_w(const svec<real_t> &wave_number, const qvec<real_t> &offsets,
+             const qvec<real_t> coords) {
   const auto n = plane_wave_n(wave_number, offsets, coords);
   return 2 * M_PI * (n(0) + n(1) + n(2) + n(3));
 }
@@ -70,16 +68,16 @@ extern "C" void TestMultiPatch_initialize(CCTK_ARGUMENTS) {
 
   const Loop::GF3D2<CCTK_REAL> gf_test_gf(layout_vc, test_gf);
 
-  const svec_u<CCTK_REAL> wave_numbers{wave_number[0], wave_number[1],
-                                       wave_number[2]};
-  const qvec_u<CCTK_REAL> offsets{time_offset, space_offset[0], space_offset[1],
-                                  space_offset[2]};
+  const svec<CCTK_REAL> wave_numbers{wave_number[0], wave_number[1],
+                                     wave_number[2]};
+  const qvec<CCTK_REAL> offsets{time_offset, space_offset[0], space_offset[1],
+                                space_offset[2]};
 
   const auto loop_lambda =
       [=] ARITH_DEVICE(const Loop::PointDesc &p) ARITH_INLINE {
         const Loop::GF3D2index index(layout_vc, p.I);
 
-        const qvec_u<CCTK_REAL> coords{
+        const qvec<CCTK_REAL> coords{
             CCTK_REAL{0},
             gf_vcoordx(index),
             gf_vcoordy(index),
