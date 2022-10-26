0f643d4b752388790fdd3fc1801f6d62069b81fc
fedelopezar <fedelopezar@gmail.com>
2022-10-26 10:47:16 -0300

    AsterX: Encapsulate instance con2primIdealFluid in proper header file.         Move some functions from 2DNRNoble to Constructor (get_Ssq_Exact, etc).

--- AsterX/src/con2prim.cxx
+++ AsterX/src/con2prim.cxx
@@ -7,7 +7,7 @@
 
 #include <cmath>
 
-#include "con2prim.hxx"
+#include "con2primIdealFluid.hxx"
 namespace AsterX {
 using namespace std;
 using namespace Loop;
@@ -22,71 +22,73 @@ NUMERICAL RECIPES IN C: THE ART OF SCIENTIFIC COMPUTING
 template <typename typeEoS>
 CCTK_HOST CCTK_DEVICE void Con2Prim_2DNRNoble(
     CCTK_INT max_iter, CCTK_REAL tolf,
-    typeEoS &plasma) { // Send con2primFactory object as reference to modify it,
-                       // and because we can not instantiate abstract class
+    typeEoS &plasma)
+{ // Send con2primFactory object as reference to modify it,
+  // and because we can not instantiate abstract class
 
-  /* get Lorentz factor seed, calculated by constructor */
-  CCTK_REAL W = plasma.W_Seed;
+    /* get Lorentz factor seed, calculated by constructor */
+    CCTK_REAL W = plasma.WLorentz_Seed;
 
-  /* get Ssq from cons (exact) */
-  plasma.get_Ssq_Exact();
+    /* get pressure seed */
+    plasma.get_Press_Seed();
 
-  /* get pressure seed */
-  plasma.get_Press_Seed();
+    /* get Z seed */
+    plasma.get_Z_Seed();
 
-  /* get Z seed */
-  plasma.get_Z_Seed();
+    /* initialize unknowns for c2p, Z and vsq: */
+    CCTK_REAL x[2];
+    CCTK_REAL x_old[2];
+    x[0] = fabs(plasma.Z_Seed);
+    x[1] = (-1.0 + W * W) / (W * W);
 
-  /* initialize unknowns for c2p, Z and vsq: */
-  CCTK_REAL x[2];
-  CCTK_REAL x_old[2];
-  x[0] = fabs(plasma.Z_Seed);
-  x[1] = (-1.0 + W * W) / (W * W);
-
-  /* initialize old values */
-  x_old[0] = x[0];
-  x_old[1] = x[1];
-
-  /* Start Recovery with 2D NR Solver */
-  const CCTK_INT n = 2;
-  const CCTK_REAL dv = (1. - 1.e-15);
-  CCTK_REAL fvec[n];
-  CCTK_REAL dx[n];
-  CCTK_REAL fjac[n][n];
-
-  CCTK_REAL detjac_inv;
-  CCTK_REAL errf;
-  plasma.Failed_2DNRNoble = 1;
-  CCTK_INT k;
-  for (k = 1; k <= max_iter; k++) {
-    fvec[0] = plasma.get_2DNRNoble_f0(x[0], x[1]);
-    fvec[1] = plasma.get_2DNRNoble_f1(x[0], x[1]);
-    fjac[0][0] = plasma.get_2DNRNoble_df0dZ(x[0], x[1]);
-    fjac[0][1] = plasma.get_2DNRNoble_df0dVsq(x[0], x[1]);
-    fjac[1][0] = plasma.get_2DNRNoble_df1dZ(x[0], x[1]);
-    fjac[1][1] = plasma.get_2DNRNoble_df1dVsq(x[0], x[1]);
-    detjac_inv = 1.0 / (fjac[0][0] * fjac[1][1] - fjac[0][1] * fjac[1][0]);
-    dx[0] = -detjac_inv * (fjac[1][1] * fvec[0] - fjac[0][1] * fvec[1]);
-    dx[1] = -detjac_inv * (-fjac[1][0] * fvec[0] + fjac[0][0] * fvec[1]);
-
-    errf = 0.0;
-    for (CCTK_INT i = 0; i < n; i++) {
-      errf += fabs(fvec[i]);
-    }
-    if (errf <= tolf) {
-      plasma.Failed_2DNRNoble = 0;
-      break;
-    }
-
-    /* save old values before calculating the new */
+    /* initialize old values */
     x_old[0] = x[0];
     x_old[1] = x[1];
 
-    for (CCTK_INT i = 0; i < n; i++) {
-      x[i] += dx[i];
+    /* Start Recovery with 2D NR Solver */
+    const CCTK_INT n = 2;
+    const CCTK_REAL dv = (1. - 1.e-15);
+    CCTK_REAL fvec[n];
+    CCTK_REAL dx[n];
+    CCTK_REAL fjac[n][n];
+
+    CCTK_REAL detjac_inv;
+    CCTK_REAL errf;
+    plasma.Failed_2DNRNoble = 1;
+    CCTK_INT k;
+    for (k = 1; k <= max_iter; k++)
+    {
+        fvec[0] = plasma.get_2DNRNoble_f0(x[0], x[1]);
+        fvec[1] = plasma.get_2DNRNoble_f1(x[0], x[1]);
+        fjac[0][0] = plasma.get_2DNRNoble_df0dZ(x[0], x[1]);
+        fjac[0][1] = plasma.get_2DNRNoble_df0dVsq(x[0], x[1]);
+        fjac[1][0] = plasma.get_2DNRNoble_df1dZ(x[0], x[1]);
+        fjac[1][1] = plasma.get_2DNRNoble_df1dVsq(x[0], x[1]);
+        detjac_inv = 1.0 / (fjac[0][0] * fjac[1][1] - fjac[0][1] * fjac[1][0]);
+        dx[0] = -detjac_inv * (fjac[1][1] * fvec[0] - fjac[0][1] * fvec[1]);
+        dx[1] = -detjac_inv * (-fjac[1][0] * fvec[0] + fjac[0][0] * fvec[1]);
+
+        errf = 0.0;
+        for (CCTK_INT i = 0; i < n; i++)
+        {
+            errf += fabs(fvec[i]);
+        }
+        if (errf <= tolf)
+        {
+            plasma.Failed_2DNRNoble = 0;
+            break;
+        }
+
+        /* save old values before calculating the new */
+        x_old[0] = x[0];
+        x_old[1] = x[1];
+
+        for (CCTK_INT i = 0; i < n; i++)
+        {
+            x[i] += dx[i];
+        }
     }
-  }
-  plasma.Nit_2DNRNoble = k;
+    plasma.Nit_2DNRNoble = k;
 
   /* make sure that the new x[] is physical */
   if (x[0] < 0.0) {
@@ -232,6 +234,10 @@ template <typename typeEoS> void AsterX_Con2Prim_typeEoS(CCTK_ARGUMENTS) {
     prims[V2_CON] = saved_vely(p.I);
     prims[V3_CON] = saved_velz(p.I);
     prims[EPS] = saved_eps(p.I);
+    prims[B1] = cons[B1];
+    prims[B2] = cons[B2]; 
+    prims[B3] = cons[B3]; 
+
 
     // Construct con2primFactory object:
     typeEoS plasma_0(gamma, cons, prims, g_lo, g_up);
---
+++ AsterX/src/con2primFactory.hxx
@@ -0,0 +1,84 @@
+#ifndef CON2PRIM_HXX
+#define CON2PRIM_HXX
+#include "AMReX_GpuQualifiers.H"
+#define CCTK_DEVICE AMREX_GPU_DEVICE
+#define CCTK_HOST AMREX_GPU_HOST
+#include "utils.hxx"
+
+constexpr int D = 0;
+constexpr int S1_COV = 1;
+constexpr int S2_COV = 2;
+constexpr int S3_COV = 3;
+constexpr int TAU = 4;
+constexpr int B1 = 5;
+constexpr int B2 = 6;
+constexpr int B3 = 7;
+
+constexpr int RHO = 0;
+constexpr int V1_CON = 1;
+constexpr int V2_CON = 2;
+constexpr int V3_CON = 3;
+constexpr int V1_COV = 1;
+constexpr int V2_COV = 2;
+constexpr int V3_COV = 3;
+constexpr int EPS = 4;
+
+constexpr int TT = 0;
+constexpr int TX = 1;
+constexpr int TY = 2;
+constexpr int TZ = 3;
+constexpr int XX = 4;
+constexpr int XY = 5;
+constexpr int XZ = 6;
+constexpr int YY = 7;
+constexpr int YZ = 8;
+constexpr int ZZ = 9;
+
+#include <math.h>
+
+namespace AsterX
+{
+
+    constexpr int NCONS = 8;
+    constexpr int NPRIMS = 8;
+
+    /* Abstract class con2primFactory */
+    class con2primFactory
+    {
+    public:
+        /* The constructor must initialize the following variables */
+        CCTK_REAL ConservedVars[NCONS];      // Conserved to solve
+        CCTK_REAL PrimitiveVarsSeed[NPRIMS]; // Primitive seeds
+        CCTK_REAL PrimitiveVars[NPRIMS];     // Primitive solution
+        CCTK_REAL gcov[10], gcon[10];
+        CCTK_REAL Bsq, BiSi, Ssq;
+        CCTK_REAL WLorentz_Seed, bsq_Seed;
+        CCTK_HOST CCTK_DEVICE void get_Ssq_Exact();  // From cons (exact)
+        CCTK_HOST CCTK_DEVICE void get_Bsq_Exact();  // From cons (exact)
+        CCTK_HOST CCTK_DEVICE void get_BiSi_Exact(); // From cons (exact)
+        CCTK_HOST CCTK_DEVICE void get_WLorentz_bsq_Seeds();
+
+        /* These must be set for 2DNRNoble scheme */
+        CCTK_INT Failed_2DNRNoble;
+        CCTK_INT Nit_2DNRNoble;
+        CCTK_REAL vsq_Sol, Press_Seed, Z_Seed, Z_Sol, vsq_Seed;
+        CCTK_HOST CCTK_DEVICE void get_Press_Seed(); // From seed prims and cons
+        CCTK_HOST CCTK_DEVICE void get_Z_Seed();     // From seed prims and cons
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f0(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f1(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_Press_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdZ_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdVsq_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dZ(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dZ(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+        CCTK_HOST CCTK_DEVICE void WZ2Prim();
+    };
+}
+#endif // #ifndef CON2PRIM_HXX
+
+template <typename typeEoS>
+CCTK_HOST CCTK_DEVICE void Con2Prim_2DNRNoble(
+    CCTK_INT max_iter, CCTK_REAL tolf,
+    typeEoS &plasma);
