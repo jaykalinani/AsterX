bf2e2f7da13ea6484a75e244734f4951a77be234
Liwei Ji <jiliwei.phys@gmail.com>
2022-10-30 18:37:45 +0000

    AsterX: Clang-format con2primFactory.hxx

--- AsterX/src/con2primFactory.hxx
+++ AsterX/src/con2primFactory.hxx
@@ -36,49 +36,52 @@ constexpr int ZZ = 9;
 
 #include <math.h>
 
-namespace AsterX
-{
+namespace AsterX {
 
-    constexpr int NCONS = 8;
-    constexpr int NPRIMS = 8;
+constexpr int NCONS = 8;
+constexpr int NPRIMS = 8;
 
-    /* Abstract class con2primFactory */
-    class con2primFactory
-    {
-    public:
-        /* The constructor must initialize the following variables */
-        CCTK_REAL ConservedVars[NCONS];      // Conserved to solve
-        CCTK_REAL PrimitiveVarsSeed[NPRIMS]; // Primitive seeds
-        CCTK_REAL PrimitiveVars[NPRIMS];     // Primitive solution
-        CCTK_REAL gcov[10], gcon[10];
-        CCTK_REAL Bsq, BiSi, Ssq;
-        CCTK_REAL WLorentz_Seed, bsq_Seed;
-        CCTK_HOST CCTK_DEVICE void get_Ssq_Exact();  // From cons (exact)
-        CCTK_HOST CCTK_DEVICE void get_Bsq_Exact();  // From cons (exact)
-        CCTK_HOST CCTK_DEVICE void get_BiSi_Exact(); // From cons (exact)
-        CCTK_HOST CCTK_DEVICE void get_WLorentz_bsq_Seeds();
+/* Abstract class con2primFactory */
+class con2primFactory {
+public:
+  /* The constructor must initialize the following variables */
+  CCTK_REAL ConservedVars[NCONS];      // Conserved to solve
+  CCTK_REAL PrimitiveVarsSeed[NPRIMS]; // Primitive seeds
+  CCTK_REAL PrimitiveVars[NPRIMS];     // Primitive solution
+  CCTK_REAL gcov[10], gcon[10];
+  CCTK_REAL Bsq, BiSi, Ssq;
+  CCTK_REAL WLorentz_Seed, bsq_Seed;
+  CCTK_HOST CCTK_DEVICE void get_Ssq_Exact();  // From cons (exact)
+  CCTK_HOST CCTK_DEVICE void get_Bsq_Exact();  // From cons (exact)
+  CCTK_HOST CCTK_DEVICE void get_BiSi_Exact(); // From cons (exact)
+  CCTK_HOST CCTK_DEVICE void get_WLorentz_bsq_Seeds();
 
-        /* These must be set for 2DNRNoble scheme */
-        CCTK_INT Failed_2DNRNoble;
-        CCTK_INT Nit_2DNRNoble;
-        CCTK_REAL vsq_Sol, Press_Seed, Z_Seed, Z_Sol, vsq_Seed;
-        CCTK_HOST CCTK_DEVICE void get_Press_Seed(); // From seed prims and cons
-        CCTK_HOST CCTK_DEVICE void get_Z_Seed();     // From seed prims and cons
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f0(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f1(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_Press_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdZ_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdVsq_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dZ(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dVsq(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dZ(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dVsq(CCTK_REAL Z, CCTK_REAL Vsq);
-        CCTK_HOST CCTK_DEVICE void WZ2Prim();
-    };
-}
+  /* These must be set for 2DNRNoble scheme */
+  CCTK_INT Failed_2DNRNoble;
+  CCTK_INT Nit_2DNRNoble;
+  CCTK_REAL vsq_Sol, Press_Seed, Z_Seed, Z_Sol, vsq_Seed;
+  CCTK_HOST CCTK_DEVICE void get_Press_Seed(); // From seed prims and cons
+  CCTK_HOST CCTK_DEVICE void get_Z_Seed();     // From seed prims and cons
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f0(CCTK_REAL Z, CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_f1(CCTK_REAL Z, CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_Press_funcZVsq(CCTK_REAL Z,
+                                                     CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdZ_funcZVsq(CCTK_REAL Z, CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_dPdVsq_funcZVsq(CCTK_REAL Z,
+                                                      CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dZ(CCTK_REAL Z,
+                                                      CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df0dVsq(CCTK_REAL Z,
+                                                        CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dZ(CCTK_REAL Z,
+                                                      CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE CCTK_REAL get_2DNRNoble_df1dVsq(CCTK_REAL Z,
+                                                        CCTK_REAL Vsq);
+  CCTK_HOST CCTK_DEVICE void WZ2Prim();
+};
+} // namespace AsterX
 #endif // #ifndef CON2PRIM_HXX
 
 template <typename typeEoS>
-CCTK_HOST CCTK_DEVICE void Con2Prim_2DNRNoble(
-    CCTK_INT max_iter, CCTK_REAL tolf,
-    typeEoS &plasma);
+CCTK_HOST CCTK_DEVICE void Con2Prim_2DNRNoble(CCTK_INT max_iter, CCTK_REAL tolf,
+                                              typeEoS &plasma);
