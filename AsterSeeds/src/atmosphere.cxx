22946f6bcc224edce8993eec391a1ed84a845709
Jay Kalinani <jaykalinani@gmail.com>
2024-07-25 19:59:05 -0400

    AsterSeeds: pass as pointer

--- AsterSeeds/src/atmosphere.cxx
+++ AsterSeeds/src/atmosphere.cxx
@@ -20,7 +20,7 @@ extern "C" void Atmosphere_Initialize(CCTK_ARGUMENTS) {
 
   // For all the tests, the initial data EOS is ideal gas
   // Get local eos object
-  auto eos_3p_ig = *global_eos_3p_ig;
+  auto eos_3p_ig = global_eos_3p_ig;
   if (not CCTK_EQUALS(evolution_eos, "IdealGas")) {
     CCTK_VERROR("Invalid evolution EOS type '%s'. Please, set "
                 "EOSX::evolution_eos = \"IdealGas\" in your parameter file.",
@@ -37,7 +37,7 @@ extern "C" void Atmosphere_Initialize(CCTK_ARGUMENTS) {
         vely(p.I) = 0.;
         velz(p.I) = 0.;
         press(p.I) = press_atmosphere;
-        eps(p.I) = eos_3p_ig.eps_from_valid_rho_press_ye(rho(p.I), press(p.I),
+        eps(p.I) = eos_3p_ig->eps_from_valid_rho_press_ye(rho(p.I), press(p.I),
                                                          dummy_ye);
       });
 
