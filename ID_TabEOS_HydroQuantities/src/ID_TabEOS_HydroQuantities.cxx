32ba826d32e2369ffb1c0f67f7692566514da222
Allen Wen <acw6923@rit.edu>
2024-07-26 11:56:01 -0500

    ID_TabEOS_HydroQuantities: store Y_e reader as object

--- ID_TabEOS_HydroQuantities/src/ID_TabEOS_HydroQuantities.cxx
+++ ID_TabEOS_HydroQuantities/src/ID_TabEOS_HydroQuantities.cxx
@@ -3,6 +3,8 @@
 #include <cctk_Parameters.h>
 #include <cmath>
 
+#include <AMReX.H>
+
 #include <loop_device.hxx>
 
 #include "ID_TabEOS_HydroQuantities.hxx"
@@ -11,6 +13,7 @@
 
 namespace ID_TabEOS_HydroQuantities{
 
+using namespace amrex;
 using namespace EOSX;
 using namespace Loop;
 
@@ -20,7 +23,7 @@ extern "C" void ID_TabEOS_HydroQuantities__initial_Y_e(CCTK_ARGUMENTS) {
 
   CCTK_VInfo(CCTK_THORNSTRING,"Y_e initialization is ENABLED!");
 
-	auto eos_3p_tab3d = *global_eos_3p_tab3d;
+	auto eos_3p_tab3d = global_eos_3p_tab3d;
 
   // Open the Y_e file, which should countain Y_e(rho) for the EOS table slice
   FILE *Y_e_file = fopen(Y_e_filename,"r");
@@ -32,35 +35,35 @@ extern "C" void ID_TabEOS_HydroQuantities__initial_Y_e(CCTK_ARGUMENTS) {
   }
   else {
     // Set nrho
-    const CCTK_INT nrho = eos_3p_tab3d.nrho;
+    const CCTK_INT nrho = eos_3p_tab3d->nrho;
 
-    // Set rho
-    CCTK_REAL rho_arr[nrho];
-    for(int i=0;i<nrho;i++) rho_arr[i] = exp(eos_3p_tab3d.logrho[i]);
+		Ye_reader* id_ye_reader =(Ye_reader*)The_Managed_Arena()->alloc(
+					sizeof *id_ye_reader); 
+		id_ye_reader->init(nrho, Y_e_file, eos_3p_tab3d->logrho);
 
-    // Now read in the Y_e file
-    CCTK_REAL Y_e_of_rho_arr[nrho];
-    read_1dfile__set_array(Y_e_file,Y_e_of_rho_arr);
     // Close the file
     fclose(Y_e_file);
 
     // Set interpolation stencil size
     const CCTK_INT interp_stencil_size = 5;
 
+		// Set Y_e
 		grid.loop_all<0, 0, 0>(
 			grid.nghostzones,
 			[=] CCTK_HOST(const PointDesc &p) CCTK_ATTRIBUTE_ALWAYS_INLINE {
 				if( rho(p.I) > id_rho_atm_max ) {
 					// Interpolate Y_e(rho_i) at gridpoint i
 					CCTK_REAL Y_eL;
-					interpolate_1d_quantity_as_function_of_rho(interp_stencil_size,nrho,rho_arr,Y_e_of_rho_arr,rho(p.I),&Y_eL);
+					id_ye_reader->interpolate_1d_quantity_as_function_of_rho(interp_stencil_size,nrho,rho(p.I),&Y_eL);
 					// Finally, set the Y_e gridfunction
-					Ye(p.I) = MIN(MAX(Y_eL, eos_3p_tab3d.rgye.min), eos_3p_tab3d.rgye.max);
+					Ye(p.I) = MIN(MAX(Y_eL, eos_3p_tab3d->rgye.min), eos_3p_tab3d->rgye.max);
 				}
 				else {
 					Ye(p.I) = id_Y_e_atm; 
 				}
 			});
+
+		free(id_ye_reader);
   }
 }
 
@@ -71,7 +74,7 @@ extern "C" void ID_TabEOS_HydroQuantities__initial_temperature(CCTK_ARGUMENTS){
 
   CCTK_VInfo(CCTK_THORNSTRING,"temperature initialization is ENABLED!");
 
-	auto eos_3p_tab3d = *global_eos_3p_tab3d;
+	auto eos_3p_tab3d = global_eos_3p_tab3d;
 
   // Loop over the grid, initializing the temperature
   grid.loop_all_device<0, 0, 0>(
@@ -81,7 +84,7 @@ extern "C" void ID_TabEOS_HydroQuantities__initial_temperature(CCTK_ARGUMENTS){
 				const CCTK_REAL r_pow_T			= atmo_falloff_T ? r_power_T : 0.;
 				const CCTK_REAL radius			= std::sqrt(p.x*p.x + p.y*p.y + p.z*p.z); 
 				const CCTK_REAL r_atmo      = std::max(r_atmo_min, radius);
-				const CCTK_REAL id_T_atm    = std::max(id_T_atm_max*std::pow(r_atmo / r_atmo_min, r_pow_T), eos_3p_tab3d.rgtemp.min);
+				const CCTK_REAL id_T_atm    = std::max(id_T_atm_max*std::pow(r_atmo / r_atmo_min, r_pow_T), eos_3p_tab3d->rgtemp.min);
 
 		 		temperature(p.I) = id_T_atm;
     });
@@ -105,7 +108,7 @@ extern "C" void ID_TabEOS_HydroQuantities__recompute_HydroBase_variables(CCTK_AR
 
 	CCTK_VInfo(CCTK_THORNSTRING,"Recomputing all HydroBase quantities ...");
 
-	auto eos_3p_tab3d = *global_eos_3p_tab3d;
+	auto eos_3p_tab3d = global_eos_3p_tab3d;
 
 	// Loop over the grid, recomputing the HydroBase quantities
   grid.loop_all_device<0, 0, 0>(
@@ -117,15 +120,15 @@ extern "C" void ID_TabEOS_HydroQuantities__recompute_HydroBase_variables(CCTK_AR
 				const CCTK_REAL radius			= std::sqrt(p.x*p.x + p.y*p.y + p.z*p.z); 
 				const CCTK_REAL r_atmo      = std::max(r_atmo_min, radius);
 				const CCTK_REAL r_pow				= atmo_falloff ? r_power : 0.;
-				const CCTK_REAL rho_atmL    = std::max(id_rho_atm_max*std::pow(r_atmo / r_atmo_min, r_pow), eos_3p_tab3d.rgrho.min);
+				const CCTK_REAL rho_atmL    = std::max(id_rho_atm_max*std::pow(r_atmo / r_atmo_min, r_pow), eos_3p_tab3d->rgrho.min);
 
 				if( rhoL > rho_atmL) {
 					CCTK_REAL yeL     = Ye(p.I);
 					CCTK_REAL tempL   = temperature(p.I);
-					press(p.I) = eos_3p_tab3d.press_from_valid_rho_temp_ye(rhoL, tempL, yeL);
+					press(p.I) = eos_3p_tab3d->press_from_valid_rho_temp_ye(rhoL, tempL, yeL);
 					eps(p.I) = 0.0; //TODO: eos_3p_tab3d.eps_from_valid_rho_temp_ye(rhoL, tempL, yeL);
 					if( initialize_entropy )
-						entropy(p.I) = eos_3p_tab3d.entropy_from_valid_rho_temp_ye(rhoL, tempL, yeL);
+						entropy(p.I) = eos_3p_tab3d->entropy_from_valid_rho_temp_ye(rhoL, tempL, yeL);
 				}
 				else {
 					// Reset to atmosphere
@@ -133,13 +136,13 @@ extern "C" void ID_TabEOS_HydroQuantities__recompute_HydroBase_variables(CCTK_AR
 
 					rho(p.I) = rho_atmL;
 					Ye(p.I) = id_Y_e_atm;
-					press(p.I) = eos_3p_tab3d.press_from_valid_rho_temp_ye(rho_atmL, tempL, id_Y_e_atm);
+					press(p.I) = eos_3p_tab3d->press_from_valid_rho_temp_ye(rho_atmL, tempL, id_Y_e_atm);
 					eps(p.I) = 0.0; //TODO: eos_3p_tab3d.eps_from_valid_rho_temp_ye(rho_atmL, tempL, id_Y_e_atm);
 					velx(p.I) = 0.0;
 					vely(p.I) = 0.0;
 					velz(p.I) = 0.0;
 					if( initialize_entropy )
-						entropy(p.I) = eos_3p_tab3d.entropy_from_valid_rho_temp_ye(rhoL, tempL, id_Y_e_atm);
+						entropy(p.I) = eos_3p_tab3d->entropy_from_valid_rho_temp_ye(rhoL, tempL, id_Y_e_atm);
 				}
 		});
 }
